b085bdc81b432188880b2a1ac03e2c8d2ac506c7
Markus-Goetz <murxman@gmail.com>
2012-05-10 14:13:31 +0200

    javascript module loading now down with requirejs

---
+++ backend/fuzztrees/static/lib/jquery-ui/require-jquery.js
@@ -0,0 +1,6 @@
+define([
+    "order!JQUERY_HOME/development-bundle/jquery-1.7.2",
+    "order!JQUERY_HOME/development-bundle/ui/jquery-ui-1.8.20.custom"], 
+    function() {
+        return jQuery;
+});
