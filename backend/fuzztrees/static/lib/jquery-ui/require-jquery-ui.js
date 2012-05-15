9a4f3403b2a60fbb020f17747c4f3b983580622b
Markus-Goetz <murxman@gmail.com>
2012-05-15 10:38:17 +0200

    removed requirejs, we can discuss this if you like Franki :P

--- backend/fuzztrees/static/lib/jquery-ui/require-jquery-ui.js
+++
@@ -1,5 +0,0 @@
-define(["order!JQUERY_HOME/development-bundle/jquery-1.7.2", 
-        "order!JQUERY_HOME/development-bundle/ui/jquery-ui-1.8.20.custom",
-        "order!JQUERY_HOME/development-bundle/jquery-ui-layout-1.2.0.js"], function() {
-    return jQuery;
-});
--- backend/fuzztrees/static/lib/jsplumb/require-jsplumb.js
+++
@@ -1,4 +0,0 @@
-define(["order!JQUERY_HOME/require-jquery-ui", 
-        "order!JSPLUMB_HOME/jsplumb-1.3.8.js"], function() {
-    return jsPlumb;
-});
