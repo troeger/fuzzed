9bc9ec3fd8a5f2fcd14b4046226416941a6e4786
Markus-Goetz <murxman@gmail.com>
2012-05-16 14:53:52 +0200

    Using jLayout now for editor page layout, using standard Cupertino jQuery UI Theme

---
+++ backend/fuzztrees/static/script/require-fuzzed.js
@@ -0,0 +1,30 @@
+define(function() {
+    jQuery(document).ready(function () {
+        console.log("alive");
+
+        var container = jQuery(".layout");
+        var west = jQuery(".west");
+        var east = jQuery(".east");
+
+        function relayout() {
+            container.layout({
+                type:   "border",
+                resize: false,
+                hgap:   3,
+                vgap:   3
+            });
+        }
+        relayout();
+
+        
+        west.resizable({
+            handles: "e",
+            resize:  relayout
+        })
+
+        east.resizable({
+            handles: "w",
+            resize:  relayout
+        })
+    });
+});
--- backend/fuzztrees/static/script/require.js
+++
@@ -1,5 +0,0 @@
-define([], function() {
-    jQuery(document).ready(function () {
-        console.log("Alliiiiiive");
-    });
-});
