500c6fbd4be89fabee18a85ae3aa9c663d1ac367
troeger <peter@troeger.eu>
2012-05-17 00:18:31 +0200

    Adding graph types and node types to model, API and fixture

--- backend/fuzztrees/api.py
+++ backend/fuzztrees/api.py
@@ -3,6 +3,15 @@ from fuzztrees.models import Graph
 import json
 
 def undos(request, graph_id):
+	"""
+	Fetch undo command stack from backend
+	API Request:  GET /api/graphs/[graphID]/undos, no body
+	API Response: JSON body with command array of undo stack
+
+	Tell the backend that an undo has been issued in the model
+	API Request:  POST /api/graphs/[graphID]/undos, no body
+	API Response: no body
+	"""
 	if request.is_ajax():
 		if request.method == 'GET':
 			# Fetch undo stack for the graph
@@ -13,6 +22,15 @@ def undos(request, graph_id):
 		return HttpResponseNotAllowed(['GET', 'POST']) 
 	
 def redos(request, graph_id):
+	"""
+	Fetch redo command stack from backend
+	API Request:  GET /api/graphs/[graphID]/redos, no body
+	API Response: JSON body with command array of redo stack
+
+	Tell the backend that an redo has been issued in the model
+	API Request:  POST /api/graphs/[graphID]/redos, no body
+	API Response: no body
+	"""
 	if request.is_ajax():
 		if request.method == 'GET':
 			# Fetch redo stack for the graph
@@ -21,8 +39,18 @@ def redos(request, graph_id):
 			# Perform top command on redo stack
 			return HttpResponse(status=200)
 		return HttpResponseNotAllowed(['GET', 'POST']) 
-		
+				
 def graph(request, graph_id):
+	"""
+	Add new graph in the backend
+	API Request:            POST /api/graphs
+	API Request Parameters: type=[GRAPH_TYPE]
+	API Response:           no body
+
+	Fetch serialized current graph from backend
+	API Request:  GET /api/graphs/[graphID] , no body
+	API Response: JSON body with serialized graph
+	"""
 	if request.is_ajax():
 		if request.method == 'GET':
 			# fetch graph 
@@ -42,6 +70,12 @@ def graph(request, graph_id):
 	
 	
 def nodes(request, graph_id):
+	"""
+	Add new node to graph stored in the backend
+	API Request:            POST /api/graphs/[graphID]/nodes
+	API Request Parameters: parent=[parentID], type=[NODE_TYPE]
+	API Response:           no body
+	"""
 	if request.is_ajax():
 		if request.method == 'POST':
 			if 'parent' in request.POST and 'type' in request.POST:
@@ -54,6 +88,26 @@ def nodes(request, graph_id):
 		return HttpResponseNotAllowed(['POST']) 
 	
 def node(request, graph_id, node_id):
+	"""
+	Delete node from graph stored in the backend
+	API Request:  DELETE /api/graphs/[graphID]/nodes/[nodeID], no body
+	API Response: no body
+
+	Move node to another position in the graph	
+	API Request:            POST /api/graphs/[graphID]/nodes/[nodeID] 
+	API Request Parameters: parent=[nodeID]
+	API Response:           no body
+
+	Change property of a node
+	API Request:            POST /api/graphs/[graphID]/nodes/[nodeID]
+	API Request Parameters: key=... , val=...
+	API Response:           no body
+
+	Morph node to another type
+	API Request:            POST /api/graphs/[graphID]/nodes/[nodeID]
+	API Request Parameters: type=[NODE_TYPE]
+	API Response:           no body
+	"""
 	if request.is_ajax():
 		if request.method == 'DELETE':
 			# delete node
--- backend/fuzztrees/models.py
+++ backend/fuzztrees/models.py
@@ -1,9 +1,52 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+GRAPH_TYPE = (
+    ( 1, u'Fault Tree'),
+    ( 2, u'Fuzz Tree'),	
+    ( 3, u'Reliability Block Diagram')
+)
+
+GRAPH_JS_TYPE = {
+	1: 'faulttree',
+	2: 'fuzztree',
+	3: 'rbd'
+}
+
+NODE_TYPE = (
+    ( 1, u'Basic event'),			
+    ( 2, u'Undeveloped event'),		
+    ( 3, u'House event'),			
+    ( 4, u'AND gate'),				
+    ( 5, u'OR gate'),				
+    ( 6, u'XOR gate'),				
+    ( 7, u'Priority AND gate'),		
+    ( 8, u'Voting OR gate'),		
+    ( 9, u'Inhibit gate'),			
+    (10, u'Choice event'),			
+    (11, u'Redundancy event'),		
+    (12, u'Block')					
+)
+
+NODE_JS_TYPE = {
+	 1: 'basic',
+	 2: 'undeveloped',
+	 3: 'house',
+	 4: 'and',
+	 5: 'or',
+	 6: 'xor',
+	 7: 'p-and',
+	 8: 'v-or',
+	 9: 'inhibit',
+	10: 'choice',
+	11: 'redundancy',
+	12: 'block'
+}
+
 class Graph(models.Model):
 	name = models.CharField(max_length=255)
 	owner = models.ForeignKey(User, related_name='graphs')
+	type = models.PositiveSmallIntegerField(choices=GRAPH_TYPE)
 	def __unicode__(self):
 		return self.name
 
@@ -11,11 +54,16 @@ class Node(models.Model):
 	name = models.CharField(max_length=255)
 	graph = models.ForeignKey(Graph, null=False, related_name='nodes')
 	root = models.BooleanField(default=False)
+	type = models.PositiveSmallIntegerField(choices=NODE_TYPE)
+	optional = models.BooleanField(default=False)
 	def __unicode__(self):
-		if self.root:
-			return self.name+" (root)"
+		if self.type == 1:
+			if self.root:
+				return self.name+" (root)"
+			else:
+				return self.name
 		else:
-			return self.name
+			return NODE_JS_TYPE[self.type]+"_"+str(self.pk)
 	def getChildren(self):
 		edges=self.outgoing.all()
 		if len(edges)>0:
