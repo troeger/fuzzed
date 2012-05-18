9743e588179ab03780d7766300e3f04680858dec
frankschlegel <frank@brunoair.local>
2012-05-18 16:29:53 +0200

    Moved nodes configuration to separate file.

--- backend/fuzztrees/models.py
+++ backend/fuzztrees/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from nodes_config import NODE_TYPES, nodeTypeChoices
+
 GRAPH_TYPE = (
     ( 1, u'Fault Tree'),
     ( 2, u'Fuzz Tree'),	
@@ -11,36 +13,6 @@ GRAPH_JS_TYPE = {
 	1: 'faulttree',
 	2: 'fuzztree',
 	3: 'rbd'
-}
-
-NODE_TYPE = (
-    ( 1, u'Basic event'),			
-    ( 2, u'Undeveloped event'),		
-    ( 3, u'House event'),			
-    ( 4, u'AND gate'),				
-    ( 5, u'OR gate'),				
-    ( 6, u'XOR gate'),				
-    ( 7, u'Priority AND gate'),		
-    ( 8, u'Voting OR gate'),		
-    ( 9, u'Inhibit gate'),			
-    (10, u'Choice event'),			
-    (11, u'Redundancy event'),		
-    (12, u'Block')					
-)
-
-NODE_JS_TYPE = {
-	 1: 'basic',
-	 2: 'undeveloped',
-	 3: 'house',
-	 4: 'and',
-	 5: 'or',
-	 6: 'xor',
-	 7: 'p-and',
-	 8: 'v-or',
-	 9: 'inhibit',
-	10: 'choice',
-	11: 'redundancy',
-	12: 'block'
 }
 
 class Graph(models.Model):
@@ -54,7 +26,7 @@ class Node(models.Model):
 	name = models.CharField(max_length=255)
 	graph = models.ForeignKey(Graph, null=False, related_name='nodes')
 	root = models.BooleanField(default=False)
-	type = models.PositiveSmallIntegerField(choices=NODE_TYPE)
+	type = models.PositiveSmallIntegerField(choices=nodeTypeChoices())
 	optional = models.BooleanField(default=False)
 	def __unicode__(self):
 		if self.type == 1:
@@ -63,7 +35,7 @@ class Node(models.Model):
 			else:
 				return self.name
 		else:
-			return NODE_JS_TYPE[self.type]+"_"+str(self.pk)
+			return NODE_TYPES[self.type]['type'] + "_" + str(self.pk)
 	def getChildren(self):
 		edges=self.outgoing.all()
 		if len(edges)>0:
---
+++ backend/fuzztrees/nodes_config.py
@@ -0,0 +1,65 @@
+NODE_TYPES = {
+    1: {
+        'name': u'Basic event',
+        'type': 'basic',
+        'image': 'basic_event.svg'
+    },
+    2: {
+        'name': u'Undeveloped event',
+        'type': 'undeveloped',
+        'image': 'undeveloped_event.svg'
+    },
+    3: {
+        'name': u'House event',
+        'type': 'house',
+        'image': 'house_event.svg'
+    },
+    4: {
+        'name': u'AND gate',
+        'type': 'and',
+        'image': 'and_gate.svg'
+    },
+    5: {
+        'name': u'OR gate',
+        'type': 'or',
+        'image': 'or_gate.svg'
+    },
+    6: {
+        'name': u'XOR gate',
+        'type': 'xor',
+        'image': 'xor_gate.svg'
+    },
+    7: {
+        'name': u'Priority AND gate',
+        'type': 'p-and',
+        'image': 'priority_and_gate.svg'
+    },
+    8: {
+        'name': u'Voting OR gate',
+        'type': 'v-or',
+        'image': 'voting_or_gate.svg'
+    },
+    9: {
+        'name': u'Inhibit gate',
+        'type': 'inhibit',
+        'image': 'inhibit_gate.svg'
+    },
+    10: {
+        'name': u'Choice event',
+        'type': 'choice',
+        'image': 'choice_event.svg'
+    },
+    11: {
+        'name': u'Redundancy event',
+        'type': 'redundancy',
+        'image': 'redundancy_event.svg'
+    },
+    12: {
+        'name': u'Block',
+        'type': 'block',
+        'image': 'block.svg'
+    },
+}
+
+def nodeTypeChoices():
+    return map(lambda (type, entry): (type, entry['name']), NODE_TYPES.iteritems())
