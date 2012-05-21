268e0303b7baf09295fddf0dad05a3f1dbae14d6
frankschlegel <frank@brunoair.local>
2012-05-21 14:49:18 +0200

    First node class.

--- backend/fuzztrees/static/script/require-fuzzed.js
+++ backend/fuzztrees/static/script/require-fuzzed.js
@@ -1,5 +1,8 @@
-define(function() {
+define(['./require-node'], function(Node) {
     jQuery(document).ready(function () {
+        /*
+         *  Init layout
+         */
         var layoutOptions = {
             defaults: {
                 applyDefaultStyles: false,
@@ -12,7 +15,19 @@ define(function() {
             }
         }
 
-
         var layout = $('body').layout(layoutOptions);
+
+        /*
+         *  Init jsPlumb
+         */
+        jsPlumb.importDefaults({
+            EndpointStyles: [{ fillStyle: '#225588' }, { fillStyle: '#225566' }],
+            Endpoints: [ ['Dot', {radius: 5}], ['Dot', {radius: 5}] ]
+        });
+
+
+        // XXX: test node
+        var node = new Node();
+        node.appendTo(jQuery('.ui-layout-center'));
     });
 });
---
+++ backend/fuzztrees/static/script/require-node.js
@@ -0,0 +1,21 @@
+define(function() {
+
+    function Node() {
+        this._visualRepresentation = jQuery('<div class="node"></div>')
+            .css({
+                'display': 'block',
+                'width': 50,
+                'height': 50,
+                'border': '1px solid black'
+                })
+            .draggable();
+
+    };
+
+    Node.prototype.appendTo = function(domElement) {
+        jQuery(domElement).append(this._visualRepresentation);
+    }
+
+
+    return Node;
+})
