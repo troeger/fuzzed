1e6bed4664118060cf70e37b4216193548c47824
frankschlegel <frank@brunoair.local>
2012-06-15 11:22:34 +0200

    More stubs for Graph and Node APIs.

--- backend/fuzztrees/static/script/require-backend.js
+++ backend/fuzztrees/static/script/require-backend.js
@@ -39,7 +39,85 @@ define(function () {
 
     }
 
+    /*
+         Function: addNode
+             Adds a new Node to a given Graph.
 
+         Parameters:
+             graph         - The Graph the node should be added to.
+             type          - The type of the node. See Config.Node.Types
+             parent        - The parent Node in the graph hierarchy.
+             position      - Position object containing an 'x' and an 'y' field specifying the Node's position.
+             errorCallback - [optional] Callback that gets called in case of an ajax-error.
+     */
+    Backend.prototype.addNode = function(graph, type, parent, position, errorCallback) {
+
+    }
+
+    /*
+         Function: deleteNode
+             Deletes a given Node in the backend.
+
+         Parameters:
+             node          - The Node that should be deleted.
+             errorCallback - [optional] Callback that gets called in case of an ajax-error.
+     */
+    Backend.prototype.deleteNode = function(node, errorCallback) {
+
+    }
+
+    /*
+         Function: relinkNode
+             Links the given Node to a new parent Node.
+
+         Parameters:
+             node          - The Node that should be deleted.
+             newParent     - The new parent Node in the graph hierarchy.
+             errorCallback - [optional] Callback that gets called in case of an ajax-error.
+     */
+    Backend.prototype.relinkNode = function(node, newParent, errorCallback) {
+
+    }
+
+    /*
+         Function: changeNodeProperty
+             Changes a property of a given Node.
+
+         Parameters:
+             node          - The Node that should be deleted.
+             key           - The name of the property.
+             value         - The new value for the property.
+             errorCallback - [optional] Callback that gets called in case of an ajax-error.
+     */
+    Backend.prototype.changeNodeProperty = function(node, key, value, errorCallback) {
+
+    }
+
+    /*
+         Function: changeNodePosition
+             Changes the position of a given Node.
+
+         Parameters:
+             node          - The Node that should be deleted.
+             position      - New position object (with 'x' and 'y' fields) of the node.
+             errorCallback - [optional] Callback that gets called in case of an ajax-error.
+     */
+    Backend.prototype.changeNodePosition = function(node, position, errorCallback) {
+
+    }
+
+    /*
+         Function: changeNodeType
+             Changes the type of a given Node.
+
+         Parameters:
+             node          - The Node that should be deleted.
+             newType       - The new type of the node. See Config.Node.Types
+             errorCallback - [optional] Callback that gets called in case of an ajax-error.
+     */
+    Backend.prototype.changeNodeType = function(node, newType, errorCallback) {
+
+    }
 
     return Backend;
 });
