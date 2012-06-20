9da7a926d08bca4f27c1a525be653c93be154856
frankschlegel <frank@brunoair.local>
2012-06-20 17:15:19 +0200

    Fixed connector position.

--- backend/fuzztrees/static/script/require-nodes.js
+++ backend/fuzztrees/static/script/require-nodes.js
@@ -279,8 +279,9 @@ define(['require-config', 'require-properties', 'require-backend', 'require-oop'
 
     Node.prototype._setupEndpoints = function() {
         // get upper and lower image offsets
-        var imageTopOffset    = this._nodeImage.offset().top - this._container.offset().top;
-        var imageBottomOffset = imageTopOffset + this._nodeImage.height();
+        var optionalIndicatorWrapper = jQuery(this._optionalIndicator._container);
+        var imageTopOffset    = optionalIndicatorWrapper.offset().top - this._container.offset().top;
+        var imageBottomOffset = this._nodeImage.offset().top - this._container.offset().top + this._nodeImage.height();
 
         // make node source
         if (this._maxInConnections != 0) {
