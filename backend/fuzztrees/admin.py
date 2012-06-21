d472faef191a7c91897bd18d8ed916620c6cf9f6
troeger <peter@troeger.eu>
2012-06-21 09:53:50 +0200

    Adding properties to admin interface

--- backend/fuzztrees/admin.py
+++ backend/fuzztrees/admin.py
@@ -1,6 +1,7 @@
-from fuzztrees.models import Graph, Node, Edge
+from fuzztrees.models import Graph, Node, Edge,Property
 from django.contrib import admin
 
 admin.site.register(Graph)
 admin.site.register(Node)
 admin.site.register(Edge)
+admin.site.register(Property)
