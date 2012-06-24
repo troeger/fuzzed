58b04b6d92769a18413535550a109108fbfa5d52
troeger <peter@troeger.eu>
2012-06-24 17:44:32 +0200

    Fixing date formatting

--- backend/fuzztrees/settings.py
+++ backend/fuzztrees/settings.py
@@ -31,7 +31,7 @@ TIME_ZONE = None
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'de-de'
+LANGUAGE_CODE = 'en-en'
 
 SITE_ID = 1
 
