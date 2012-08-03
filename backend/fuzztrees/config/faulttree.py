9105728e3e0ec5f2929d893c12e59826cd16ee58
frankschlegel <frank@brunoair.local>
2012-08-03 16:26:44 +0200

    Same for fauttree config.

--- backend/fuzztrees/config/faulttree.py
+++ backend/fuzztrees/config/faulttree.py
@@ -2,215 +2,188 @@ FAULTTREE_CONFIG = {
 	'nodes': {
 		'node': {
 			'excludeFromShapesMenu': True,
+			'name': u'Node',
 		    'numberOfIncomingConnections': -1, #infinite
 		    'numberOfOutgoingConnections':  1,
 		    'allowConnectionTo': ['node'],
-		    'name': {
-			    'type': 'text',
-		        'displayName': u'Name',
-			    'default': u'Node'
-		    }
+			'propertiesMenuEntries': {
+				'name': {
+					'type': 'text',
+					'displayName': u'Name'
+				},
+				'cost': {
+					'type': 'number',
+					'displayName': u'Cost',
+					'min': 0,
+					'step': 1,
+					'disabled': True #only editable for basic events and sets
+				},
+				'probability': {
+					'type': 'text',
+					'displayName': u'Probability',
+					'disabled': True
+				},
+				'cardinality': {
+					'displayName': u'Cardinality',
+					'type': 'number',
+					'min': 1,
+					'step': 1
+				},
+				'count': {
+					'type': 'number',
+					'displayName': u'Count',
+					'min': 1,
+					'step': 1,
+				}
+			}
 		},
 
 	    'event': {
 		    'inherits': 'node',
+			'name': u'Event',
+			'probability': '',
 		    'excludeFromShapesMenu': True,
 	        'numberOfIncomingConnections':  1,
 	        'numberOfOutgoingConnections': -1,
-	        'allowConnectionTo': ['gate'],
-	        'name': {
-		        # events show their names below the image
-		        'mirror': {
-			        'position': 'bottom',
-			        'style': ['bold']
-		        },
-	            'default': u'Event'
-	        },
-	        'probability': {
-		        'type': 'text',
-	            'displayName': u'Probability',
-	            'default': '',
-	            'disabled': True
-	        }
+	        'allowConnectionTo': ['gate']
 	    },
 
 	    'gate': {
 		    'inherits': 'node',
+			'name': u'Gate',
 		    'excludeFromShapesMenu': True,
 	        'numberOfIncomingConnections': -1,
-	        'numberOfOutgoingConnections':  1,
-	        'name': {
-		        'default': u'Gate'
-	        }
+	        'numberOfOutgoingConnections':  1
 	    },
 
 	    'basicEvent': {
 		    'inherits': 'event',
+			'name': u'Basic Event',
+			'probability': 0,
 			'excludeFromShapesMenu': False,
 		    'numberOfIncomingConnections': 0,
 			'image': 'basic_event.svg',
 	        'help': 'Initiating failure in a basic component',
-	        'name': {
-		        'default': u'Basic Event'
-	        },
-	        'probability': {
-				'type': 'number',
-				'displayname': u'Exact',
-	            'min': 0,
-	            'max': 1,
-	            'step': 0.01,
-	            'default': 0
-	        },
+			'propertiesMenuEntries': {
+				'probability': {
+					'type': 'number',
+					'min': 0,
+					'max': 1,
+					'step': 0.01,
+					'disabled': False
+				}
+			}
 	    },
 
 	    'basicEventSet': {
 		    'inherits': 'basicEvent',
+			'name': u'Basic Event Set',
+			'cardinality': 1,
+			'excludeFromShapesMenu': False,
 		    'image': 'basic_event_set.svg',
-		    'help': 'Set of basic events with identical properties',
-		    'name': {
-			    'default': u'Basic Event Set'
-		    },
-	        'cardinality': {
-		        'displayName': u'Cardinality',
-	            'type': 'number',
-	            'min': 1,
-	            'default': 1,
-	            'step': 1
-	        }
+		    'help': 'Set of basic events with identical properties'
 	    },
 
 	    'intermediateEvent': {
 		    'inherits': 'event',
+			'name': u'Intermediate Event',
 			'excludeFromShapesMenu': False,
 		    'image': 'intermediate_event.svg',
-		    'help': 'Failure resulting from a combination of previous events',
-		    'name': {
-			    'default': u'Intermediate Event'
-		    }
+		    'help': 'Failure resulting from a combination of previous events'
 	    },
 
 	    'intermediateEventSet': {
 		    'inherits': 'intermediateEvent',
+			'name': u'Intermediate Event Set',
+			'cardinality': 1,
+			'excludeFromShapesMenu': False,
 		    'numberOfIncomingConnections': 0,
 		    'image': 'intermediate_event_set.svg',
-		    'help': 'Set of intermediate events',
-		    'name': {
-			    'default': u'Intermediate Event Set'
-		    },
-		    'cardinality': {
-			    'displayName': u'Cardinality',
-			    'type': 'number',
-			    'min': 1,
-			    'default': 1,
-			    'step': 1
-		    }
+		    'help': 'Set of intermediate events'
 	    },
 
 		'undevelopedEvent': {
 			'inherits': 'event',
+			'name': u'Undeveloped Event',
 			'excludeFromShapesMenu': False,
 			'numberOfIncomingConnections': 0,
 			'image': 'undeveloped_event.svg',
-			'help': 'Event with no information available or insignificant impact',
-			'name': {
-				'default': u'Undeveloped Event'
-			}
+			'help': 'Event with no information available or insignificant impact'
 		},
 
 		'houseEvent': {
 			'inherits': 'event',
+			'name': u'House Event',
 			'excludeFromShapesMenu': False,
 			'numberOfIncomingConnections': 0,
 			'image': 'house_event.svg',
-			'help': 'An event that is expected to occur and typically does not denote a failure',
-			'name': {
-				'default': u'House Event'
-			}
+			'help': 'An event that is expected to occur and typically does not denote a failure'
 		},
 
 		'topEvent': {
 			'inherits': 'event',
+			'name': u'Top Event',
 			'image': 'intermediate_event.svg',
-			'excludeFromShapesMenu': True,
-		    'optional': None,
-		    'name': {
-			    'default': u'Top Event'
-		    }
+			'excludeFromShapesMenu': True
 		},
 
 	    'andGate': {
 		    'inherits': 'gate',
+			'name': u'AND Gate',
 			'excludeFromShapesMenu': False,
 		    'image': 'and_gate.svg',
-		    'help': 'Output event occurs if all input events occur',
-		    'name': {
-			    'default': u'AND Gate'
-		    }
+		    'help': 'Output event occurs if all input events occur'
 	    },
 
 	    'priorityAndGate': {
 		    'inherits': 'gate',
+			'name': u'Priority AND Gate',
 			'excludeFromShapesMenu': False,
 		    'image': 'priority_and_gate.svg',
-		    'help': 'Output event occurs if all input events occur in the specific order',
-		    'name': {
-			    'default': u'Priority AND Gate'
-		    }
+		    'help': 'Output event occurs if all input events occur in the specific order'
 	    },
 
 	    'orGate': {
 		    'inherits': 'gate',
+			'name': u'OR Gate',
 			'excludeFromShapesMenu': False,
 		    'image': 'or_gate.svg',
-		    'help': 'Output event occurs if one or more input events occur',
-		    'name': {
-			    'default': u'OR Gate'
-		    }
+		    'help': 'Output event occurs if one or more input events occur'
 	    },
 
 	    'xorGate': {
 		    'inherits': 'gate',
+			'name': u'XOR Gate',
 			'excludeFromShapesMenu': False,
 		    'image': 'xor_gate.svg',
-		    'help': 'Output event occurs if exactly one of the input events occur',
-		    'name': {
-			    'default': u'XOR Gate'
-		    }
+		    'help': 'Output event occurs if exactly one of the input events occur'
 	    },
 
 	    'votingOrGate': {
 		    'inherits': 'gate',
+			'name': u'Voting OR Gate',
+			'count': 2,
 			'excludeFromShapesMenu': False,
 		    'image': 'voting_or_gate.svg',
-		    'help': 'Output event occurs if the given number of input events occur',
-		    'name': {
-			    'default': u'Voting OR Gate'
-		    },
-	        'count': {
-		        'type': 'number',
-	            'displayName': u'Count',
-	            'min': 1,
-	            'step': 1,
-	            'default': 2
-	        }
+		    'help': 'Output event occurs if the given number of input events occur'
 	    },
 
 	    'inhibitGate': {
 		    'inherits': 'gate',
+			'name': u'Inhibit Gate',
 			'excludeFromShapesMenu': False,
 		    'image': 'inhibit_gate.svg',
-		    'help': 'Output event occurs if the single input event occurs and the enabling condition is given',
-		    'name': {
-			    'default': u'Inhibit Gate'
-		    }
+		    'help': 'Output event occurs if the single input event occurs and the enabling condition is given'
 	    }
 
 	},
 
-    'propertyDisplayOrder': [
+    'propertiesDisplayOrder': [
 		'name',
         'cost',
         'probability',
+		'cardinality',
+		'count'
     ],
 
     'shapeMenuNodeDisplayOrder': [
