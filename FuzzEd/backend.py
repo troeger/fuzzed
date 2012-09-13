3e2e35f609f703a069eb841a5b0176b9bc75a686
troeger <peter@troeger.eu>
2012-09-13 15:00:58 +0200

    Fixing cut set determination

--- FuzzEd/backend.py
+++ FuzzEd/backend.py
@@ -4,6 +4,7 @@ class MinBool(ast.NodeVisitor):
     def __init__(self):
         self.current=[]
         self.result=[]
+        self.inOr=False
 
     def simplify(self, boolterm):
         res = minbool.simplify(boolterm)
@@ -11,19 +12,31 @@ class MinBool(ast.NodeVisitor):
         return self.result
 
     def generic_visit(self, arg):
+        #print "Arg: "+str(arg)
         super(MinBool, self).generic_visit(arg)
+        #print "Current: "+str(self.current)
         if self.current != []:
             self.result.append({'nodes':self.current})
-        self.current=[]
+            self.current=[]
 
     def visit_Name(self, name):
         self.current.append(str(name.id))
 
     def visit_Num(self, num):
-        self.current.append(num.n)
+        #print "Num: "+str(num)
+        if self.inOr:
+            #print "inor"
+            self.result.append({'nodes':[num.n]})
+        else:
+            #print "inand"
+            self.current.append(num.n)
 
     def visit_Or(self, boolop):
+        #print "OR"
+        self.inOr=True
         self.generic_visit(boolop)
 
     def visit_And(self, boolop):
+        #print "AND"
+        self.inOr=False
         self.generic_visit(boolop)
