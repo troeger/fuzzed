72f408a9272015b484b539d38e1d1ccf01f23bff
Markus-Goetz <murxman@gmail.com>
2013-01-17 15:36:42 +0100

    Complete documentation for the Canvas class. Added event On: descriptions in editor and graph.

--- FuzzEd/static/script/canvas.js
+++ FuzzEd/static/script/canvas.js
@@ -1,18 +1,39 @@
 define(['class', 'config', 'jquery.svgdom', 'jquery.ui/jquery.ui.droppable', 'jquery.ui/jquery.ui.selectable'],
 function(Class, Config) {
+    /**
+     * Package: Base
+     */
 
     /**
-     *  Class: Canvas
-     *      TODO
+     *  Class: {Singleton} Canvas
+     *
+     *  This singleton class models the canvas entity. It is mainly responsible for two things: painting the background
+     *  grid and providing coordinate conversion functions (pixel to grid coordinates and vice versa). The canvas is a
+     *  the drop and selectable target for <Nodes>.
+     *
      *  Events:
      *      Config.Events.CANVAS_SHAPE_DROPPED - An SVG element was dropped onto the canvas.
      */
-    var Canvas = Class.extend({
-        container: undefined,
-        gridSize:  Config.Grid.SIZE,
+    return new Class.extend({
+        /**
+         * Group: Members
+         *
+         * {DOMElement} container   - The canvas container element - i.e. DOM element with id <Config::IDs::CANVAS>.
+         * {Number}     gridSize    - Size in pixels of one canvas grid cell (default: <Config::Grid::SIZE>.
+         *
+         * {DOMElement} _background - SVG DOM element where the dashed grid lines are drawn on.
+         */
+        container:   undefined,
+        gridSize:    Config.Grid.SIZE,
 
         _background: undefined,
 
+        /**
+         * Constructor: init
+         *
+         * Creates a new instance of the <Canvas> class. Invokes permanent side effects on the DOM and should therefore
+         * only be called once.
+         */
         init: function() {
             // locate predefined DOM elements and bind Editor instance to canvas
             this.container = jQuery('#' + Config.IDs.CANVAS);
@@ -21,11 +42,28 @@ function(Class, Config) {
                 ._setupCanvas();
         },
 
-        /* Section: Coordinate conversion */
+        /**
+         * Section: Coordinate conversion
+         */
 
+        /**
+         * Method: toGrid
+         *
+         * Converts the passed pixel coordinates to their respective grid coordinates. This method will look for the
+         * closest grid coordinate, if the pixel coordinates do not represent a "grid crossing".
+         *
+         * Parameters:
+         *   {Object|Number} first  - Either an object of the form {'x': ..., 'y': ...} representing the complete pixel
+         *                            coordinates or a single number standing for the pixel x coordinate.
+         *   {Number}        second - If first is not an object, a number standing for the pixel y coordinate or assumed
+         *                            to be undefined.
+         *
+         * Returns:
+         *   An {Object} containing the 'x' and 'y' grid coordinates of the passed pixel coordinates.
+         */
         toGrid: function(first, second) {
-            var x = Number.NaN;
-            var y = Number.NaN;
+            var x = window.Number.NaN;
+            var y = window.Number.NaN;
 
             // if both parameter are numbers we can take them as they are
             if (_.isNumber(first) && _.isNumber(second)) {
@@ -40,14 +78,29 @@ function(Class, Config) {
             }
 
             return {
-                x: Math.round(x / this.gridSize),
-                y: Math.round(y / this.gridSize)
+                x: window.Math.round(x / this.gridSize),
+                y: window.Math.round(y / this.gridSize)
             }
         },
 
+        /**
+         * Method: toPixel
+         *
+         * Converts the passed grid coordinates to their pixel equivalent. Does NOT implement snap behaviour to the
+         * closest grid crossing.
+         *
+         * Parameters:
+         *   {Object|Number} first  - Either an object of the form {'x': ..., 'y': ...} representing the complete grid
+         *                            coordinates or a single number standing for the grid's x coordinate.
+         *   {Number}        second - If the parameter first is an object assumed to be undefined, otherwise a number
+         *                            standing for the grid's y coordinate.
+         *
+         * Returns:
+         *   An {Object} containing the 'x' and 'y' pixel coordinates of the passed grid coordinates.
+         */
         toPixel: function(first, second) {
-            var x = Number.NaN;
-            var y = Number.NaN;
+            var x = window.Number.NaN;
+            var y = window.Number.NaN;
 
             if (_.isNumber(first) && _.isNumber(second)) {
                 x = first;
@@ -64,8 +117,20 @@ function(Class, Config) {
             }
         },
 
-        /* Section: Internals */
+        /**
+         *  Section: Initialization
+         */
 
+        /**
+         * Methid: _drawGrid
+         *
+         * Draws the dashed grid line on the <Canvas::_background> DOM element. Clears any previous lines from the
+         * background first (needed in case of window resizing). The lines are drawn from top to bottom and left to
+         * right, using SVG lines.
+         *
+         * Returns:
+         *   This {<Canvas>} instance for chaining.
+         */
         _drawGrid: function() {
             var height = this.container.height();
             var width  = this.container.width();
@@ -100,15 +165,41 @@ function(Class, Config) {
             return this;
         },
 
+        /**
+         * Method: _setupBackground
+         *
+         * Creates the _background SVG DOM element, draws the grid initially for the first time and ensures that the
+         * grid is refreshed on window resizing.
+         *
+         * Returns:
+         *   This {<Canvas>} instance for chaining.
+         */
         _setupBackground: function() {
             this._background = this.container.svg().svg('get');
             this._drawGrid();
-            // redraw the background grid when the window is being resized
+            // on window resize, redraw grid
             jQuery(window).resize(this._drawGrid.bind(this));
 
             return this;
         },
 
+        /**
+         * Method: _setupCanvas
+         *
+         * This method sets the canvas up to be a drop and select target. The first allows that shapes from the shapes
+         * menu can be dragged and dropped onto the canvas. Whereas the latter is responsible to allow the user to make
+         * rectangular boxes for multi selects. While setting up the two targets, this method also registers callbacks
+         * to handle un- and highlighting on selection and to re-trigger the custom events stated below.
+         *
+         * Returns:
+         *   This {<Canvas>} instance for chaining.
+         *
+         * Triggers:
+         *   <Config::Events::CANVAS_SHAPE_DROPPED>
+         *   <Config::Events::CANVAS_EDGE_SELECTED>
+         *   <Config::Events::CANVAS_EDGE_UNSELECTED>
+         *   <Config::Events::CANVAS_SELECTION_STOPPED>
+         */
         _setupCanvas: function() {
             // make canvas droppable for shapes from the shape menu
             this.container.droppable({
@@ -125,28 +216,33 @@ function(Class, Config) {
             this.container.selectable({
                 filter: '.' + Config.Classes.NODE + ', .' + Config.Classes.JSPLUMB_CONNECTOR,
                 selecting: function(event, ui) {
+                    // highlight nodes...
                     var selection = jQuery(ui.selecting);
                     if (selection.hasClass(Config.Classes.NODE)) {
                         selection.data(Config.Keys.NODE).select();
                     }
 
+                    // ... and edges that are part of the new selection
                     if (selection.hasClass(Config.Classes.JSPLUMB_CONNECTOR)) {
                         var edgeId = selection.attr(Config.Attributes.CONNECTION_ID);
                         jQuery(document).trigger(Config.Events.CANVAS_EDGE_SELECTED, edgeId);
                     }
                 },
                 unselecting: function(event, ui) {
+                    // unhighlight nodes...
                     var unselection = jQuery(ui.unselecting);
                     if (unselection.hasClass(Config.Classes.NODE)) {
                         unselection.data(Config.Keys.NODE).deselect();
                     }
 
+                    // ... and edges when the selection is cleared
                     if (unselection.hasClass(Config.Classes.JSPLUMB_CONNECTOR)) {
                         var edgeId = unselection.attr(Config.Attributes.CONNECTION_ID);
                         jQuery(document).trigger(Config.Events.CANVAS_EDGE_UNSELECTED, edgeId);
                     }
                 },
-                stop: function(event, ui) {
+                stop: function() {
+                    // tell other (e.g. <PropertyMenu>) that selection is done and react to the new selection
                     jQuery(document).trigger(Config.Events.CANVAS_SELECTION_STOPPED);
                 }
             });
@@ -154,6 +250,4 @@ function(Class, Config) {
             return this;
         }
     });
-
-    return new Canvas();
 });
