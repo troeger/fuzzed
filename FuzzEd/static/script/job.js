83c0dcd99a25028d7e588d91adaf0dd328496fed
frankschlegel <frank@brunoair.local>
2013-02-13 12:00:47 +0100

    Changes in Job interface that allows to set callbacks and intervalls after initialization.

--- FuzzEd/static/script/job.js
+++ FuzzEd/static/script/job.js
@@ -10,20 +10,21 @@ define(["class"], function(Class) {
          *  Group: Members
          *
          *  Properties:
-         *    {String}      _url                - The query URL of this job.
-         *    {Function}    _successCallback    - Function that is called as soon as the job is finished.
-         *    {Function}    _updateCallback     - Function that is called each time the job progress is queried and
-         *                                        the job progress is reported back as 'not finished'.
-         *    {Function}    _errorCallback      - Function that is called if the job results in an error.
-         *    {int}         _queryInterval      - The interval (in ms) between job queries.
-         *    {Timeout)     _timeout            - A reference to the current job query timeout.
+         *    {Function}    successCallback    - Function that is called as soon as the job is finished.
+         *    {Function}    updateCallback     - Function that is called each time the job progress is queried and
+         *                                       the job progress is reported back as 'not finished'.
+         *    {Function}    errorCallback      - Function that is called if the job results in an error.
+         *    {int}         queryInterval      - The interval (in ms) between job queries.
+         *    {String}      _url               - The query URL of this job.
+         *    {Timeout)     _timeout           - A reference to the current job query timeout.
          */
-        _url:             undefined,
-        _successCallback: jQuery.noop(),
-        _updateCallback:  jQuery.noop(),
-        _errorCallback:   jQuery.noop(),
-        _queryInterval:   0,
-        _timeout:         undefined,
+        successCallback: jQuery.noop(),
+        updateCallback:  jQuery.noop(),
+        errorCallback:   jQuery.noop(),
+        queryInterval:   0,
+
+        _url:            undefined,
+        _timeout:        undefined,
 
         /**
          *  Group: Initialization
@@ -35,22 +36,22 @@ define(["class"], function(Class) {
          *
          *  Parameters:
          *    {String}      url                - The query URL of this job.
-         *    {int}         queryInterval      - The interval (in ms) between job queries.
+         *    {int}         queryInterval      - [optional] The interval (in ms) between job queries. Defaults to 1000.
          *    {Function}    successCallback    - [optional] Function that is called as soon as the job is finished.
          *    {Function}    updateCallback     - [optional] Function that is called each time the job progress is
          *                                       queried and the job progress is reported back as 'not finished'.
          *    {Function}    errorCallback      - [optional] Function that is called if the job results in an error.
-         *    {boolean}     startImmediately   - [optional] Start querying this Job after init. Default is 'true'.
+         *    {boolean}     startImmediately   - [optional] Start querying this Job after init. Default is 'false'.
          *
          */
         init: function(url, queryInterval, successCallback, updateCallback, errorCallback, startImmediately) {
-            if (typeof startImmediately === 'undefined') startImmediately = true;
+            if (typeof startImmediately === 'undefined') startImmediately = false;
 
-            this._url             = url;
-            this._successCallback = successCallback || jQuery.noop();
-            this._updateCallback  = updateCallback  || jQuery.noop();
-            this._errorCallback   = errorCallback   || jQuery.noop();
-            this._queryInterval   = queryInterval;
+            this._url            = url;
+            this.successCallback = successCallback || jQuery.noop();
+            this.updateCallback  = updateCallback  || jQuery.noop();
+            this.errorCallback   = errorCallback   || jQuery.noop();
+            this.queryInterval   = queryInterval   || 1000;
 
             if (startImmediately) this.start();
         },
@@ -90,17 +91,17 @@ define(["class"], function(Class) {
                 url: this._url,
                 statusCode: {
                     200: function(data) { // success
-                        this._successCallback(data);
+                        this.successCallback(data);
                     }.bind(this),
 
                     202: function(data) { // not finished
-                        this._updateCallback(data);
+                        this.updateCallback(data);
                         // query again later
-                        this._timeout = setTimeout(this._query, this._queryInterval);
+                        this._timeout = setTimeout(this._query, this.queryInterval);
                     }.bind(this),
 
                     404: function() { // not found / canceled
-                        this._errorCallback();
+                        this.errorCallback();
                     }.bind(this)
                 }
             });
