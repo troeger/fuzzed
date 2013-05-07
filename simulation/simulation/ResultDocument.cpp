952725aae3fcd0a38e4078bee2f0fa3145cf6b14
laena <lenaherscheid@gmail.com>
2013-05-07 12:09:37 +0200

    xml for simulation result- initial work

---
+++ simulation/simulation/ResultDocument.cpp
@@ -0,0 +1,56 @@
+#include "ResultDocument.h"
+#include "Constants.h"
+
+using namespace pugi;
+using std::string;
+using namespace simulation;
+
+ResultDocument::ResultDocument() : xml_document()
+{
+	initXML();
+}
+
+void ResultDocument::initXML()
+{
+	m_root = append_child(SIMULATION_RESULT);
+}
+
+void ResultDocument::addError(const string& msg)
+{
+	// TODO
+}
+
+void ResultDocument::addWarning(const std::string& message)
+{
+	// TODO
+}
+
+void ResultDocument::setModelId(const int& modelID)
+{
+	m_root.append_attribute(MODELID).set_value(modelID);
+}
+
+void ResultDocument::setTimeStamp(const int& timeStamp)
+{
+	m_root.append_attribute(TIMESTAMP).set_value(timeStamp);
+}
+
+void ResultDocument::setResults(
+	const long double& reliability, 
+	const long double& meanAvailability, 
+	const long double& mttf, 
+	const int& nRounds, 
+	const int& nFailures)
+{
+	m_root.append_attribute(RELIABILITY).set_value((double)reliability);
+	m_root.append_attribute(AVAILABILTIY).set_value((double)meanAvailability);
+	m_root.append_attribute(MTTF).set_value((double)mttf);
+	m_root.append_attribute(NROUNDS).set_value(nRounds);
+	m_root.append_attribute(NFAILURES).set_value(nFailures);
+}
+
+bool ResultDocument::save(const string& fileName)
+{
+	m_bSaved = xml_document::save_file(fileName.c_str());
+	return m_bSaved;
+}
---
+++ simulation/simulation/ResultDocument.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#if IS_WINDOWS 
+#pragma warning(push, 3) 
+#endif
+#include "pugixml.hpp"
+#include <vector>
+#if IS_WINDOWS 
+#pragma warning(pop)
+#endif
+
+class SimulationResult;
+
+class ResultDocument : public pugi::xml_document
+{
+public:
+	ResultDocument();
+	virtual ~ResultDocument() {}
+
+	void addError(const std::string& message);
+	void addWarning(const std::string& message);
+
+	void setModelId(const int& modelID);
+	void setTimeStamp(const int& timeStamp);
+	void setResults(
+		const long double& reliability, 
+		const long double& meanAvailability, 
+		const long double& mttf,
+		const int& nRounds,
+		const int& nFailures);
+
+	bool save(const std::string& fileName);
+
+	bool valid() const { return !pugi::xml_document::empty(); }
+	bool saved() const { return m_bSaved; }
+
+protected:
+	void initXML();
+
+	pugi::xml_node m_root;
+	bool m_bSaved;
+
+	int m_warnings;
+	int m_errors;
+};
