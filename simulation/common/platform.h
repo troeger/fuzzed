042ca4a5b5777beae12f5851638c43515a2bc311
laena <lenaherscheid@gmail.com>
2013-05-16 11:07:20 +0200

    windows cmake configuration

--- simulation/common/platform.h
+++ simulation/common/platform.h
@@ -7,11 +7,17 @@
 #endif
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+
 #ifdef SIMULATION_DLL
 	#define FT_DLL_API __declspec(dllexport)
 #else
 	#define FT_DLL_API __declspec(dllimport)
 #endif
+
+#ifdef SIMULATION_STATIC
+	#define FT_DLL_API 
+#endif
+
 #else
 	#define FT_DLL_API
 #endif
