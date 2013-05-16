b4b8e7ea928ceb9001d4ccce548d4e1369016287
laena <lenaherscheid@gmail.com>
2013-05-16 11:06:48 +0200

    preliminary XOR implementation

--- simulation/modeltransform/gates/XORGate.cpp
+++ simulation/modeltransform/gates/XORGate.cpp
@@ -4,3 +4,31 @@
 XORGate::XORGate(const std::string& ID, const std::string& name)
 	: Gate(ID, name)
 {}
+
+int XORGate::serialize(boost::shared_ptr<PNDocument> doc) const 
+{
+	vector<int> childIDs;
+	for (auto it = getChildrenBegin(); it != getChildrenEnd(); ++it)
+		childIDs.push_back((*it)->serialize(doc));
+
+	int oneChildFailed			= doc->addPlace(0, 100, "XOR_Failed");
+	int moreThanOneChildFailed	= doc->addPlace(0, 0, "XOR_moreThanOne");
+	for (int id : childIDs)
+	{
+		if (id < 0)
+		{
+			cout << "Invalid child found, ID: " << id << endl;
+			continue;
+		}
+		int propagateChildFailure = doc->addImmediateTransition();
+
+		doc->placeToTransition(id, propagateChildFailure);
+		doc->transitionToPlace(propagateChildFailure, oneChildFailed);
+	}
+
+	int discardMultipleFailures = doc->addImmediateTransition(2.0, Condition(), "more than one child failed");
+	doc->placeToTransition(oneChildFailed, discardMultipleFailures, 2); // TODO: this should be ">=2". Or add transitions for everi i<numChildren.
+	doc->transitionToPlace(discardMultipleFailures, moreThanOneChildFailed, 0);
+
+	return oneChildFailed;
+}
--- simulation/modeltransform/gates/XORGate.h
+++ simulation/modeltransform/gates/XORGate.h
@@ -6,4 +6,6 @@ class XORGate : public Gate
 public:
 	XORGate(const std::string& ID, const std::string& name);
 	virtual ~XORGate(){};
+
+	virtual int serialize(boost::shared_ptr<PNDocument> doc) const override;
 };
