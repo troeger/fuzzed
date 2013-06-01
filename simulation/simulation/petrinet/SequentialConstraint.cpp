df8a4be0268380276d934e7158e668b7781c73e7
laena <lenaherscheid@gmail.com>
2013-06-01 08:51:52 +0200

    sequence constraints on timed transitions

--- simulation/simulation/petrinet/SequentialConstraint.cpp
+++ simulation/simulation/petrinet/SequentialConstraint.cpp
@@ -3,8 +3,9 @@
 
 using namespace std;
 
-SequentialConstraint::SequentialConstraint(const vector<string>& sequence)
-	: m_requiredSequence(sequence), 
+SequentialConstraint::SequentialConstraint(const vector<string>& sequence, SequenceType type) :
+	m_requiredSequence(sequence),
+	m_type(type),
 	m_satisfied(true), 
 	m_sequencePos(0)
 {}
@@ -12,6 +13,22 @@ SequentialConstraint::SequentialConstraint(const vector<string>& sequence)
 bool SequentialConstraint::isSatisfied(const PetriNet* const pn)
 {
 	assert(m_satisfied);
+
+	switch (m_type)
+	{
+	case STATIC_TRANSITIION_SEQ: 
+		return checkTransitionSequence(pn); 
+	case DYNAMIC_PLACE_SEQ:
+		return checkPlaceSequence(pn);
+	default:
+		assert(false);
+	}
+
+	return checkPlaceSequence(pn);
+}
+
+bool SequentialConstraint::checkPlaceSequence(const PetriNet* const pn)
+{
 	for (const auto& placeMarking : pn->m_placeDict)
 	{
 		if (placeMarking.second.getCurrentMarking() > 0)
@@ -24,10 +41,28 @@ bool SequentialConstraint::isSatisfied(const PetriNet* const pn)
 					return false;
 				}
 
-			for (int i = m_sequencePos; i != m_requiredSequence.size(); ++i)
-				if (m_requiredSequence.at(i) == placeID)
-					m_sequencePos = i;
+				for (int i = m_sequencePos; i != m_requiredSequence.size(); ++i)
+					if (m_requiredSequence.at(i) == placeID)
+						m_sequencePos = i;
 		}
 	}
 	return true;
+}
+
+bool SequentialConstraint::checkTransitionSequence(const PetriNet* const pn)
+{
+	auto sequencePos = m_requiredSequence.begin();
+	for (const auto& p : pn->m_activeTimedTransitions)
+	{
+		const string transitionID = p.second->getID();
+		for (auto it = m_requiredSequence.begin(); it != m_requiredSequence.end(); ++it)
+		{ // find this transition
+			if (it->getID() == transitionID)
+			{
+				if (sequencePos > it) return false;
+				sequencePos = it;
+			}
+		}
+	
+	}
 }
--- simulation/simulation/petrinet/SequentialConstraint.h
+++ simulation/simulation/petrinet/SequentialConstraint.h
@@ -3,13 +3,25 @@
 #include <vector>
 #include <string>
 
+class PetriNet;
+
+enum SequenceType
+{
+	STATIC_TRANSITIION_SEQ,
+	DYNAMIC_PLACE_SEQ
+};
+
 class SequentialConstraint
 {
 public:
-	SequentialConstraint(const std::vector<std::string>& sequence);
+	SequentialConstraint(const std::vector<std::string>& sequence, SequenceType type);
 	bool isSatisfied(const PetriNet* const pn);
 
 private:
+	bool checkPlaceSequence(const PetriNet* const pn);
+	bool checkTransitionSequence(const PetriNet* const pn);
+
+	SequenceType m_type;
 	std::vector<std::string> m_requiredSequence;
 	int m_sequencePos;
 
