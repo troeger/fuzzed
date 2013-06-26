e55443858a943fd23c3d093e07d661686d1d3948
troeger <peter@troeger.eu>
2013-06-26 14:12:04 +0200

    Adding beanstalk, consolidating backend startup

---
+++ FuzzEd/management/commands/runbackend.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand, CommandError
+from analysis import top_event_probability
+from subprocess import Popen, PIPE
+import os, sys
+
+def printAnalysisServerStatus():
+	print 'Job\tStatus'
+	print '---\t------'
+
+	for job_id, status in top_event_probability.list_jobs().iteritems():
+		print '%s\t%s' % (job_id, status)	
+
+class Command(BaseCommand):
+	help = 'Starts the backend services'
+
+	def handle(self, *args, **options):
+		print "Starting beanstalkd messaging server ..."
+		beanstalk = Popen(["beanstalkd"], stdout=PIPE, stderr=PIPE)
+		if beanstalk.returncode != None:
+			print "Error %u while starting beanstalkd"%beanstalk.returncode
+			exit(-1)
+		print "Starting Java analysis server ..."
+		analysis = Popen(["java","-jar","analysis/jar/fuzzTreeAnalysis.jar","-runServer"], stderr=PIPE, stdout=PIPE)
+		if analysis.returncode != None:
+			print "Error %u while starting Java analysis server"%analysis.returncode
+			beanstalk.terminate()
+			exit(-1)
+		while 1:
+			print "Press any key for status information, or 'q' for quitting ..."
+			line = sys.stdin.readline()
+			if line.startswith('q'):
+				analysis.terminate()
+				beanstalk.terminate()
+				exit(0)
+			else:
+				printAnalysisServerStatus()
+				print 79*"-"
+
+
--- FuzzEd/management/commands/runcalcserver.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-import os
-
-class Command(BaseCommand):
-	help = 'Starts the analysis engine remotely used by the FuzzEd backend'
-
-	def handle(self, *args, **options):
-		os.system("java -jar analysis/jar/fuzzTreeAnalysis.jar -runServer")
