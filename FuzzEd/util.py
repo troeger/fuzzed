a3be90774284d7ad4d433a7eb5ef0b892aef424e
troeger <peter@troeger.eu>
2013-06-27 17:00:29 +0200

    Fixing default name bug

--- FuzzEd/util.py
+++ FuzzEd/util.py
@@ -4,12 +4,13 @@ def extend(target, source, *others, **options):
 
     for other in all_sources:
         if not deep:
+            # perform classical dict update, since nested dicts are not used
             target.update(other)
             continue
 
         for key, value in other.items():
             if key in target and isinstance(target[key], dict) and isinstance(value, dict):
-                target[key] = extend(target[key], other[key], deep=True)
+                target[key] = extend({}, target[key], other[key], deep=True)
             else:
                 target[key] = value
 
