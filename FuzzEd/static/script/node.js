198cd23f2d71b73898853bc3a4fb8b18697f23b6
Markus-Goetz <murxman@gmail.com>
2013-07-04 18:35:08 +0200

    Fixed font-awesome node edge plus icon issue

--- FuzzEd/static/script/node.js
+++ FuzzEd/static/script/node.js
@@ -193,7 +193,7 @@ function(Property, Mirror, Canvas, Class) {
          */
         _setupConnectionHandle: function() {
             if (this.numberOfOutgoingConnections != 0) {
-                this._connectionHandle = jQuery('<i class="icon-plus icon-white"></i>')
+                this._connectionHandle = jQuery('<i class="icon-plus icon-justify"></i>')
                     .addClass(this.config.Classes.NODE_HALO_CONNECT + ' ' + this.config.Classes.NO_PRINT)
                     .css({
                         'top':  this._nodeImage.yCenter + this._nodeImage.outerHeight(true) / 2,
