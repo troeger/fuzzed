37b5efc320b87bc3c62836cf8ad8a244b8b7614a
laena <lenaherscheid@gmail.com>
2013-09-06 12:15:47 +0200

    configuring to build pugixml via cmake

--- backends/cmake/Findpugixml.cmake
+++ backends/cmake/Findpugixml.cmake
@@ -4,9 +4,8 @@ find_path(PUGIXML_ROOT src/pugiconfig.hpp
 )
 
 find_path(PUGIXML_INCLUDE_DIR pugiconfig.hpp ${PUGIXML_ROOT}/src)
+find_library(PUGIXML_LIB pugixml PATHS ${PUGIXML_ROOT}/lib)
 
-if(WIN32)
-	find_library(PUGIXML_LIB_RELEASE pugixml PATHS ${PUGIXML_ROOT}/lib)
-	find_library(PUGIXML_LIB_DEBUG pugixmld PATHS ${PUGIXML_ROOT}/lib)
-	message("Using Pugixml as a lib")
-endif(WIN32)
+if (NOT PUGIXML_LIB OR NOT PUGIXML_INCLUDE_DIR)
+	message(FATAL_ERROR "Could not find pugixml.")
+endif()
