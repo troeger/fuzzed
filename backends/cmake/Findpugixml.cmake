5cdc729f1453b9d548575b774a57c0aa99a99b74
laena <lenaherscheid@gmail.com>
2013-09-06 22:50:23 +0200

    fixing pugixml/beanstalkpp find scripts and lots of other stuff

--- backends/cmake/Findpugixml.cmake
+++ backends/cmake/Findpugixml.cmake
@@ -4,9 +4,21 @@ find_path(PUGIXML_ROOT src/pugiconfig.hpp
 )
 
 find_path(PUGIXML_INCLUDE_DIR pugiconfig.hpp ${PUGIXML_ROOT}/src)
-find_library(PUGIXML_LIB 
-	NAMES pugixml libpugixml
-	PATHS ${PUGIXML_ROOT}/lib)
+
+find_library(PUGIXML_LIB_DEBUG
+  NAMES pugixmld pugixml
+  PATHS PATHS ${PUGIXML_ROOT}/lib
+  )
+
+find_library(PUGIXML_LIB_RELEASE
+  NAMES pugixml
+  PATHS PATHS ${PUGIXML_ROOT}/lib
+  )
+
+set(PUGIXML_LIB
+  debug ${PUGIXML_LIB_DEBUG}
+  optimized ${PUGIXML_LIB_RELEASE}
+  )
 
 if (NOT PUGIXML_LIB OR NOT PUGIXML_INCLUDE_DIR)
 	message(FATAL_ERROR "Could not find pugixml.")
