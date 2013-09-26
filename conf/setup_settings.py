067f1920191ba75aeb1cb846d1744c402663d491
troeger <peter@troeger.eu>
2013-09-26 15:24:39 +0200

    Working on central settings.ini support

---
+++ conf/setup_settings.py
@@ -0,0 +1,127 @@
+settings='''
+import os.path, logging
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
+%(conf_lines)s
+
+DATABASES = {
+    'default': {
+        'ENGINE':   'django.db.backends.%(db_type)s', 
+        'NAME':     '%(db_name)s',                        
+        'USER':     '%(db_user)s',                              
+        'PASSWORD': '%(db_password)s',                       
+        'HOST':     '',                                       
+        'PORT':     '',                                       
+    }
+}
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    #'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    %(add_static_finders)s
+)
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    #'django.template.loaders.eggs.Loader',
+
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.static',
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+    %(add_context_processors)s
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    #Uncomment the next line for simple clickjacking protection:
+    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    %(add_middleware)s
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    %(add_installed_apps)s
+)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level':   'DEBUG',
+            'class':   'logging.StreamHandler'
+        },
+        'mail_admins': {
+            'level':   'ERROR',
+            'class':   'django.utils.log.AdminEmailHandler'
+        },        
+        'False': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers':  ['%(logger_requests)s'],
+            'level':     'ERROR',
+            'propagate': True,
+        },
+        'FuzzEd': {
+            'handlers':  ['%(logger_fuzzed)s'],
+            'level':     'DEBUG',
+            'propagate': True,
+        }
+    }
+}
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    %(add_auth_backends)s
+)
+'''
+
+import ConfigParser
+
+def createDjangoSettings(addSection):
+    ''' Parse settings.ini and fill the upper template.
+        The result is a valid Django setttings.py file,
+        based on the configuration from the INI file.
+        The argument tells the special section in the
+        INI file that is considered beside 'all'.
+    '''
+    conf = ConfigParser.ConfigParser()
+    conf.optionxform = str   # preserve case in keys
+    conf.read('settings.ini')
+    replacements = {}
+    conf_lines = []
+    for section in ('all', addSection):
+        for key, value in conf.items(section):
+            if key.isupper():
+                # Add the configuration from the INI file directly
+                conf_lines.append('%s=%s'%(key, value))
+            else:
+                # Register it as replacement value
+                replacements[key] = value
+    replacements['conf_lines']='\n'.join(sorted(conf_lines))
+    return settings%replacements
+
+print createDjangoSettings('development')
