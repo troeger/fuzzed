c5bd1bdf619aecd41fa93898071fd669c310146b
troeger <peter@troeger.eu>
2013-10-04 23:14:41 +0200

    Add missing file

---
+++ fabfile/build/__init__.py
@@ -0,0 +1,249 @@
+import os, json, pprint
+from xml.dom.minidom import parse as parseXml
+from setup_schemas import createFaultTreeSchema, createFuzzTreeSchema
+from setup_settings import createDjangoSettings, createBackendSettings
+from fabric.api import task
+from FuzzEd import util
+# check FuzzEd/__init__.py for the project version number
+# from FuzzEd import __version__, util, settings
+
+
+def svg2pgf_shape(filename):
+    '''
+        Convert given SVG file to TiKZ code.
+    '''
+    xml = parseXml(filename)
+    # determine size of the picture from SVG source
+    svg = xml.getElementsByTagName('svg')[0]
+    name = svg.attributes['id'].value
+    height = int(svg.attributes['height'].value)
+    width = int(svg.attributes['width'].value)
+    # Define shape anchors, based on image size
+    # We need double backslashes since the output is Python again
+    # The SVG coordinate system is mirrored on the horizon axis, so we add a rotation command and a positional compensation
+    result = '''
+\\\\pgfdeclareshape{%(name)s}{
+    \\\\anchor{center}{\pgfpoint{%(halfwidth)u}{%(halfheight)u}}
+    \\\\anchor{north}{\pgfpoint{%(halfwidth)u}{%(height)u}}
+    \\\\anchor{south}{\pgfpoint{%(halfwidth)u}{0}}
+    \\\\anchor{west}{\pgfpoint{0}{%(halfheight)u}}
+    \\\\anchor{east}{\pgfpoint{%(width)u}{%(halfheight)u}}
+    \\\\foregroundpath{
+        \\\\pgfsetlinewidth{1.4}
+        \\\\pgftransformshift{\pgfpoint{%(width)u}{%(height)u}}
+        \\\\pgftransformrotate{180} 
+        \\\\pgfsetfillcolor{white}
+'''%{'name':name, 'height':height, 'halfheight':height/2, 'width':width, 'halfwidth':width/2}
+    # add all SVG path
+    pathCommands = xml.getElementsByTagName('path')
+    for p in pathCommands:
+        # The path may have styling. We ignore everything but dashing.
+        if p.attributes.has_key('style'):
+            if 'stroke-dasharray' in p.attributes['style'].value:
+                # http://stuff.mit.edu/afs/athena/contrib/tex-contrib/beamer/pgf-1.01/doc/generic/pgf/version-for-tex4ht/en/pgfmanualse23.html
+                result += "        \\\\pgfsetdash{{4.2}{1.4}}{0}\n"
+        # Add the SVG path
+        result +="        \\\\pgfpathsvg{%s}\n"%p.attributes['d'].value
+    # add all SVG rectangle definitions
+    # Add usepath after each rectangle, in order to get overlayed filled rects correctly generated
+    rectCommands = xml.getElementsByTagName('rect')
+    for r in rectCommands:
+        rheight = float(r.attributes['height'].value)
+        rwidth = float(r.attributes['width'].value)
+        x = float(r.attributes['x'].value)
+        y = float(r.attributes['y'].value)
+        result += "        \\\\pgfrect{\pgfpoint{%f}{%f}}{\pgfpoint{%f}{%f}}\n\\\\pgfusepath{stroke, fill}\n"%(x, y, rwidth, rheight)
+    # add all SVG circle definitions
+    circleCommands = xml.getElementsByTagName('circle')
+    for c in circleCommands:
+        x = float(c.attributes['cx'].value)
+        y = float(c.attributes['cy'].value)
+        radius = float(c.attributes['r'].value)
+        result += "        \\\\pgfcircle{\pgfpoint{%f}{%f}}{%f}\n\\\\pgfusepath{stroke, fill}\n"%(x,y,radius)
+    # finalize TiKZ shape definition
+    result += '        \\\\pgfusepath{stroke}\n}}'
+    return result
+
+def build_shape_lib_recursive(startdir='FuzzEd/static/img', covered=[]):
+    ''' 
+        Build static LaTex representation for our graphical symbols as TiKZ shapes.
+        Some SVGs occur multiple times in subdirectories, so we track the already
+        converted ones. 
+    '''
+    result = ''
+    for root, dirs, files in os.walk(startdir):
+        for d in dirs:
+            result += build_shape_lib_recursive(root+d,covered)
+        for f in files:
+            if f.endswith('.svg') and f not in covered:
+                try:
+                    result += svg2pgf_shape(root+os.sep+f)
+                    covered.append(f)
+                    print "Converting %s to TiKZ shape ..."%f
+                except Exception, e:
+                    print "Error on parsing, ignoring %s ..."%f
+                    print e
+    return result
+
+def generate_choices(notations):
+    return [(notation['kind'], notation['name']) for notation in notations]
+
+def generate_node_choices(notations):
+    node_choices = []
+
+    for notation in notations:
+        nodes = notation['nodes']
+        node_category = (notation['name'],)
+        node_category_choices = ()
+
+        for node_kind, node in nodes.items():
+            node_category_choices += ((node_kind, node['nodeDisplayName']),)
+
+        node_category += (node_category_choices,)
+        node_choices.append(node_category)
+
+    return node_choices
+
+def resolve_inheritance(notations):
+    for notation in notations:
+        nodes = notation['nodes']
+        node_cache = {}
+
+        for node_name, node in nodes.items():
+            nodes[node_name] = inherit(node_name, node, nodes, node_cache)
+
+def inherit(node_name, node, nodes, node_cache):
+    inherits_from = node.get('inherits')
+
+    if not inherits_from:
+        node_cache[node_name] = node
+        return node
+
+    elif inherits_from not in node_cache:
+        inherit(inherits_from, nodes[inherits_from], nodes, node_cache)
+
+    resolved = util.extend({}, node_cache[inherits_from], node, deep=True)
+    node_cache[node_name] = resolved
+
+    return resolved
+
+
+@task
+def shapes():
+    '''Builds TiKZ shape library needed for Latex export / rendedering server.'''
+    print "Generating TiKZ shape library ..."
+    f=open("FuzzEd/models/node_rendering.py","w")
+    f.write("# Auto-generated by 'setup.py build', do not change !\n")
+    f.write("tikz_shapes='''")
+    f.write("\n%% Start of shape library. This part remains the same for all graph exports.")
+    f.write(build_shape_lib_recursive())
+    f.write("\n%% End of shape library. This part below is unique for all graph exports.\n")    
+    f.write("'''")
+    f.close()
+
+@task
+def notations():
+    '''Builds Python equivalents of notation JSON files.'''
+    notations_dir = 'FuzzEd/static/notations/'
+    file_list = os.listdir(notations_dir)
+    notations = []
+
+    for file_path in [os.path.join(notations_dir, file_name) for file_name in file_list]:
+        if os.path.isfile(file_path) and file_path.endswith('.json'):
+
+            with open(file_path) as handle:
+                notations.append(json.loads(handle.read()))
+    resolve_inheritance(notations)
+
+    out=open('FuzzEd/models/notations.py', 'w')
+    out.write('# DO NOT EDIT! This file is auto-generated by "setup.py build"\n')
+    out.write('notations = ')
+    pprint.pprint(notations, out)
+    out.write('\nby_kind = {notation[\'kind\']: notation for notation in notations}\n')
+    out.write('choices = ')
+    pprint.pprint(generate_choices(notations), out)
+    out.write('\nnode_choices = ')
+    pprint.pprint(generate_node_choices(notations), out)
+    out.write('\n# END OF GENERATED CONTENT')
+
+def build_xmlschema_wrappers():
+    print 'Building XML schema wrappers ...'
+
+    IS_WINDOWS = os.name == 'nt'
+    FILE_EXTENSION = '.py' if IS_WINDOWS else ''
+    FILE_PREFIX = 'file:///' + os.getcwd() + '/' if IS_WINDOWS else '' 
+
+    # Remove old binding files and generate new ones
+    for file_name in ['xml_faulttree.py', 'xml_fuzztree.py', 'xml_analysis.py']:
+        path_name = 'FuzzEd/models/%s' % file_name
+
+        if os.path.exists(path_name):
+            os.remove(path_name)
+    if os.system('pyxbgen%s --binding-root=FuzzEd/models/ -u %sFuzzEd/static/xsd/analysis.xsd '
+                 '-m xml_analysis -u %sFuzzEd/static/xsd/fuzztree.xsd -m xml_fuzztree -u %sFuzzEd/static/xsd/faulttree.xsd -m xml_faulttree'
+                 % (FILE_EXTENSION, FILE_PREFIX, FILE_PREFIX, FILE_PREFIX,)) != 0:
+        raise Exception('Execution of pyxbgen failed.\nTry "sudo setup.py test" for installing all dependencies.')
+
+@task
+def xmlschemas():
+    '''Builds XML schema files and according Python wrappers.'''
+    print "Generating XML schema files ..."
+    createFaultTreeSchema("FuzzEd/static/xsd/faulttree.xsd")
+    createFuzzTreeSchema("FuzzEd/static/xsd/fuzztree.xsd")
+    build_xmlschema_wrappers()
+
+@task
+def naturaldocs():
+    '''Builds source code documentation in /docs.'''
+    # Build natural docs in 'docs' subdirectory
+    if not os.path.exists('docs'):
+        os.mkdir('docs')
+    if os.system('tools/NaturalDocs/NaturalDocs -i FuzzEd -i analysis -i rendering -xi FuzzEd/static/lib -o HTML docs -p docs ') != 0:
+        raise Exception('Execution of NaturalDocs compiler failed.')
+
+@task 
+def configs():
+    '''Builds the configuration files for sub projects out of settings.ini'''
+    print 'Building configs...'
+    f=open('FuzzEd/settings.py','w')
+    f.write(createDjangoSettings('settings.ini', 'development'))
+    f.close()
+    f=open('backends/database.ini','w')
+    f.write(createBackendSettings('settings.ini', 'development'))
+    f.close()
+
+def build_analysis_server_java():
+    '''Builds analysis server.'''
+    print 'Building analysis server JAR file ...'
+    current = os.getcwd()
+    os.chdir('analysis/jar')
+
+    if os.system('ant clean') != 0:
+        raise Exception('Execution of ANT failed. Is it installed?')
+
+    if os.system('ant') != 0:
+        raise Exception('Execution of ANT failed. Please check the previous output.')
+
+    os.chdir(current)
+
+@task
+def backend_servers():
+    '''Builds configuration and analysis server with CMAKE / C++11 compiler.'''
+    print 'Building backend servers ...'
+    current = os.getcwd()
+    os.chdir('backends')
+    if os.path.isfile("CMakeCache.txt"):
+        os.system("rm CMakeCache.txt") 
+    os.system("cmake .")
+    os.system('make all')
+    os.chdir(current)
+
+@task()
+def all():
+    '''Builds all.'''
+    shapes()
+    xmlschemas()
+    naturaldocs()
+    notations()
+    configs()
+    backend_servers()    
