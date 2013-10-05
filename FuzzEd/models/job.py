c340cdc0fbf815963ab100ab7272fdefb78e3a9b
troeger <peter@troeger.eu>
2013-10-05 12:14:46 +0200

    Intermediate brokeen version

--- FuzzEd/models/job.py
+++ FuzzEd/models/job.py
@@ -7,16 +7,27 @@ class Job(models.Model):
 
     CUTSETS_JOB   = 'C'
     TOP_EVENT_JOB = 'T'
+    EPS_RENDERING_JOB = 'E'
+    PDF_RENDERING_JOB = 'P'    
+    STATE_NEW = 'N'
+    STATE_FETCHED = 'F'
+    STATE_DONE = 'D'
 
     JOB_TYPES = (
         (CUTSETS_JOB,   'Cutset computation'),
-        (TOP_EVENT_JOB, 'Top event calculation')
+        (TOP_EVENT_JOB, 'Top event calculation'),
+        (EPS_RENDERING_JOB, 'EPS rendering job'),
+        (PDF_RENDERING_JOB, 'PDF rendering job')
     )    
 
+    STATE_TYPES = (
+        (STATE_NEW, 'Job was not fetched so far.'),
+        (STATE_FETCHED, 'Job was fetched.'),
+        (STATE_DONE, 'Result is available.'))
+
     graph = models.ForeignKey(Graph, null=False, related_name='jobs')
-    name  = models.CharField(max_length=255)
+    name  = models.CharField(max_length=255, null=True)
     kind  = models.CharField(max_length=127, choices=JOB_TYPES)
+    state = models.CharField(max_length=127, choices=STATE_TYPES, default=STATE_NEW)
     created = models.DateTimeField(auto_now_add=True, editable=False)
-    configurations = models.IntegerField(default=0)
-    nodes = models.IntegerField(default=0)
-    
+    result = models.FileField('jobs')    
