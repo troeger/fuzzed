886fc3995de81193bc66414bebef147424fccf10
laena <lenaherscheid@gmail.com>
2013-10-15 17:24:26 +0200

    using CommandLineParser in analysis main

--- backends/fuzztreeanalysis/main.cpp
+++ backends/fuzztreeanalysis/main.cpp
@@ -1,66 +1,27 @@
 #include "InstanceAnalysisTask.h"
 #include "AlphaCutAnalysisTask.h"
-#include "AnalysisResult.h"
+#include "AnalysisResultDocument.h"
 
 #include <string>
-#include <boost/filesystem/path.hpp>
-#include <boost/filesystem/operations.hpp>
-#include <boost/program_options.hpp>
 #include <iostream>
 #include <fstream>
 
+#include "CommandLineParser.h"
 #include "util.h"
 
-namespace po = boost::program_options;
-namespace fs = boost::filesystem;
 
 using std::endl;
-using std::cout;
+using std::cerr;
 using std::string;
 
 int main(int argc, char** argv)
 {
-	// const std::string testfile = "C:\\dev\\fuzztrees\\backends\\fuzztreeanalysis\\testdata\\EWDC2013.fuzztree"; // "C:\\dev\\fuzztrees\\backends\\simulation\\testdata\\mixed.fuzztree";
 	try
 	{
-		string outDir, inFile;
-		bool generateFaultTree;
-
-		po::options_description options;
-		options.add_options()
-			("help,h", "produce help message")
-			("infile,i",		po::value<string>(&inFile),		"Path to FuzzTree file")
-			("outdir,o",		po::value<string>(&outDir),		"Output directory for FuzzTree or faulttree files")
-			("faulttree,f",		po::value<bool>(&generateFaultTree));
-
-		po::variables_map optionsMap;
-		po::store(po::parse_command_line(argc, argv, options), optionsMap);
-		po::notify(optionsMap);
-
-		if (optionsMap.count("help")) 
-		{
-			cout << options << endl;
-			return -1;
-		}
-		else if (optionsMap.count("infile") == 0 || optionsMap.count("outdir") == 0)
-		{
-			cout << "Please specify input file and output directory." << endl;
-			cout << options << endl;
-			return -1;
-		}
-
-		const auto dirPath = fs::path(outDir.c_str());
-		const auto inFilePath = fs::path(inFile.c_str());
-		if (!fs::is_directory(dirPath))
-		{ // TODO: find out write permissions. not featured by Boost 1.48.
-			cout << "Not a valid directory name: " << dirPath << endl;
-			return -1;
-		}
-		else if (!fs::is_regular_file(inFilePath))
-		{
-			cout << "Not a valid file name: " << inFile << endl;
-			return -1;
-		}
+		CommandLineParser parser;
+		parser.parseCommandline(argc, argv);
+		const auto inFile = parser.getInputFilePath().generic_string();
+		const auto outFile = parser.getOutputFilePath().generic_string();
 
 		std::ifstream file(inFile);
 		auto t = fuzztree::fuzzTree(file, xml_schema::Flags::dont_validate);
@@ -68,19 +29,16 @@ int main(int argc, char** argv)
 
 		auto topEvent = fuzztree::TopEvent(t->topEvent());
 		
-		const int dn = 10; // TODO where does this come from?
+		const int decompositionNumber = 10; // TODO where does this come from?
 
-		InstanceAnalysisTask* analysis = new InstanceAnalysisTask(&topEvent, dn);
+		InstanceAnalysisTask* analysis = new InstanceAnalysisTask(&topEvent, decompositionNumber);
 		const auto result = analysis->compute();
 
-		std::string xmlFile = util::fileNameFromPath(inFile);
-		util::replaceFileExtensionInPlace(xmlFile, ".xml");
-
-		AnalysisResult resultDocument;
+		AnalysisResultDocument resultDocument;
 		resultDocument.setModelId(t->id());
-		resultDocument.setDecompositionNumber(dn);
+		resultDocument.setDecompositionNumber(decompositionNumber);
 		resultDocument.addConfiguration(result);
-		resultDocument.save(outDir + "/" + xmlFile);
+		resultDocument.save(outFile);
 	}
 	catch (const std::exception& e)
 	{
