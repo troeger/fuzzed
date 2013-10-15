d6dbbcd0eb1b0ec1c9c51cbec8b74becd6434fe3
laena <lenaherscheid@gmail.com>
2013-10-15 17:18:03 +0200

    adding commandlineparser class

---
+++ backends/common/CommandLineParser.cpp
@@ -0,0 +1,73 @@
+#include "CommandLineParser.h"
+
+#include <boost/filesystem/operations.hpp>
+#include <string>
+#include <iostream>
+
+#include "util.h"
+
+namespace po = boost::program_options;
+namespace fs = boost::filesystem;
+
+CommandLineParser::CommandLineParser()
+{
+	m_commands.add_options()
+		("help,h", "output help message"),
+		("verbose,v", po::value<bool>(&m_bVerbose), "run in verbose mode");
+}
+
+CommandLineParser::CommandLineParser(boost::program_options::options_description& additionalOptions)
+{
+	m_commands.add(additionalOptions);
+	m_commands.add_options()
+		("help,h", "output help message"),
+		("verbose,v", po::value<bool>(&m_bVerbose), "run in verbose mode");
+}
+
+void CommandLineParser::parseCommandline(int numArguments, char** arguments)
+{
+	po::store(po::parse_command_line(numArguments, arguments, m_commands), m_optionsMap);
+	po::notify(m_optionsMap);
+	if (m_optionsMap.count("help")) 
+	{
+		std::cerr << m_commands << std::endl;
+		exit(-1);
+	}
+
+	// Positional options without leading letter identifiers
+	// #1: input file. usually FuzzTree XML
+	// #2: output path. for result documents
+
+	else if (numArguments != 3)
+	{
+		std::cerr << "Faulty command line options. Use [program_name] [inputfile] [outputfile]" << std::endl;
+		exit(-1);
+	}
+	std::string outFile, inFile;
+	inFile = arguments[1];
+	outFile = arguments[2];
+
+	const auto outFilePath	= fs::path(outFile.c_str());
+	const auto inFilePath	= fs::path(inFile.c_str());
+
+	if (!util::isWritable(outFile))
+	{ // TODO: find out write permissions. not featured by Boost 1.48.
+		std::cerr << "Cannot write to file: " << outFilePath << std::endl;
+		exit(-1);
+	}
+	else if (!fs::is_regular_file(inFilePath))
+	{
+		std::cerr << "Not a valid file name: " << inFile << std::endl;
+		exit(-1);
+	}
+}
+
+const boost::filesystem::path& CommandLineParser::getInputFilePath() const
+{
+	return m_inFilePath;
+}
+
+const boost::filesystem::path& CommandLineParser::getOutputFilePath() const
+{
+	return m_outFilePath;
+}
---
+++ backends/common/CommandLineParser.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <boost/filesystem/path.hpp>
+#include <boost/program_options.hpp>
+
+/************************************************************************/
+/* Parses the command line for an input and output path specification.	*/
+/* Exits the application on invalid options								*/
+/************************************************************************/
+
+class CommandLineParser
+{
+public:
+	CommandLineParser();
+	CommandLineParser(boost::program_options::options_description& additionalOptions);
+
+	const boost::filesystem::path& getInputFilePath() const;
+	const boost::filesystem::path& getOutputFilePath() const;
+	const bool& isVerbose() const;
+
+	void parseCommandline(int numArguments, char** arguments);
+
+protected:
+	boost::filesystem::path m_inFilePath;
+	boost::filesystem::path m_outFilePath;
+	bool m_bVerbose;
+
+	boost::program_options::variables_map					m_optionsMap;
+	boost::program_options::options_description				m_commands;
+};
