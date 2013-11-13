6a7c4f1c1386b9d7c8389a97431e59eb579af48e
Markus-Goetz <murxman@gmail.com>
2013-11-13 18:26:23 +0100

    * Made configurations work in front end again   - Parsing the backend result XML in Job.py   - Reverse engineered the old JSON format   - Mapping to this format including client ids * There is a gotcha here, I am doing hard instance checks for FeatureChoice, …   - Normally nothing bad with that in python   - However, it feels like PyXB should have something not introspective for that   - Is there a better way to do this?

--- FuzzEd/models/job.py
+++ FuzzEd/models/job.py
@@ -10,13 +10,15 @@ from FuzzEd import settings
 from FuzzEd.middleware import HttpResponseServerErrorAnswer
 import uuid, json
 
+from xml_configurations import FeatureChoice, InclusionChoice, RedundancyChoice, TransferInChoice
+
 import logging
 logger = logging.getLogger('FuzzEd')
 
 class NativeXmlField(models.Field):
     def db_type(self, connection):
         return 'xml'
-add_introspection_rules([], ["^FuzzEd\.models\.job\.NativeXmlField"])        
+add_introspection_rules([], ['^FuzzEd\.models\.job\.NativeXmlField'])
 
 def gen_uuid():
     return str(uuid.uuid4())
@@ -27,25 +29,25 @@ class Job(models.Model):
 
     # These strings must be lowercase, since they travel through PostgreSQL notification,
     # which makes everything lower case
-    CUTSETS_JOB   = 'cutsets'
-    TOP_EVENT_JOB = 'topevent'
+    CUTSETS_JOB       = 'cutsets'
+    TOP_EVENT_JOB     = 'topevent'
     EPS_RENDERING_JOB = 'eps'
     PDF_RENDERING_JOB = 'pdf'    
 
     JOB_TYPES = (
-        (CUTSETS_JOB,   'Cutset computation'),
-        (TOP_EVENT_JOB, 'Top event calculation'),
+        (CUTSETS_JOB,       'Cutset computation'),
+        (TOP_EVENT_JOB,     'Top event calculation'),
         (EPS_RENDERING_JOB, 'EPS rendering job'),
         (PDF_RENDERING_JOB, 'PDF rendering job')
     )
 
-    graph = models.ForeignKey(Graph, null=True, related_name='jobs')
-    graph_modified = models.DateTimeField()                                # Detect graph changes during job execution
-    secret  = models.CharField(max_length=64, default=gen_uuid)            # Unique secret for this job
-    kind  = models.CharField(max_length=127, choices=JOB_TYPES)
-    created = models.DateTimeField(auto_now_add=True, editable=False)
-    result = models.FileField(upload_to='jobs', null=True)                 # Result file for this job
-    exit_code = models.IntegerField(null=True)                             # Exit code for this job, NULL if pending
+    graph          = models.ForeignKey(Graph, null=True, related_name='jobs')
+    graph_modified = models.DateTimeField()                                  # Detect graph changes during job execution
+    secret         = models.CharField(max_length=64, default=gen_uuid)       # Unique secret for this job
+    kind           = models.CharField(max_length=127, choices=JOB_TYPES)
+    created        = models.DateTimeField(auto_now_add=True, editable=False)
+    result         = models.FileField(upload_to='jobs', null=True)           # Result file for this job
+    exit_code      = models.IntegerField(null=True)                          # Exit code for this job, NULL if pending
 
     def input_data(self):
         ''' Used by the API to get the input data needed for the particular job type.'''
@@ -65,35 +67,71 @@ class Job(models.Model):
 
     def result_rendering(self):
         ''' Returns the job result as something that the frontend understands.'''
-        json_result = {"errors": {},"warnings": {}}
+        json_result = {
+            'errors': {},
+            'warnings': {}
+        }
         json_config = []
+        result_data = ''
+
         try:
             assert(self.kind == Job.TOP_EVENT_JOB)
             assert(self.result)
-            resultdata = ''.join(self.result.readlines())
-            configurations = xml_analysis.CreateFromDocument(resultdata).result
-            # This will move to the right position in an upcoming schema update
-            json_result["decompositionNumber"]=str(configurations[0].decompositionNumber)
-            json_result["timestamp"]=configurations[0].timestamp
-            json_result["validResult"]=str(configurations[0].validResult)
-            for config in configurations:
-                current_config = {}
-                if hasattr(config.configuration, "costs"):
-                    current_config["costs"] = config.configuration.costs
-                json_alphacuts = {}
-                for ac in config.probability.alphaCuts:
-                    json_alphacuts[ac.key] = [ac.value_.lowerBound, ac.value_.upperBound]
-                current_config["alphaCuts"] = json_alphacuts
-                current_config["choices"] = {}
-                json_config.append(current_config)
-            json_result["configurations"]=json_config
-            returndata = json.dumps(json_result)
-            logger.debug("Returning result JSON to frontend:\n"+returndata) 
-            return returndata
-        except Exception as e:
-            mail_managers("Error on analysis result XML->JSON conversion",str(resultdata)+"\n\n"+str(e))
-            raise HttpResponseServerErrorAnswer()
 
+            result_data    = ''.join(self.result.readlines())
+            configurations = xml_analysis.CreateFromDocument(result_data).result
+
+            # This will move to the right position in an upcoming schema update
+            json_result['decompositionNumber'] = str(configurations[0].decompositionNumber)
+            json_result['timestamp']           = configurations[0].timestamp
+            json_result['validResult']         = str(configurations[0].validResult)
+
+            for config in configurations:
+                # get the cost from the xml
+                current_config = {}
+                if hasattr(config.configuration, 'costs'):
+                    current_config['costs'] = config.configuration.costs
+
+                # fetch the alphacuts
+                json_alphacuts = {}
+                for alpha_cut in config.probability.alphaCuts:
+                    json_alphacuts[alpha_cut.key] = [
+                        alpha_cut.value_.lowerBound,
+                        alpha_cut.value_.upperBound
+                    ]
+                current_config['alphaCuts'] = json_alphacuts
+
+                # tell something about the choices
+                json_choices = {}
+                for choice in config.configuration.choice:
+                    element     = choice.value_
+                    json_choice = {}
+
+                    #TODO: is there a better way to do this with PyXB?
+                    if isinstance(element, FeatureChoice):
+                        json_choice['type']      = 'FeatureChoice'
+                        json_choice['featureId'] = self.graph.nodes.get(id=element.featureId).client_id
+                    elif isinstance(element, InclusionChoice):
+                        json_choice['type']     = 'InclusionChoice'
+                        json_choice['included'] = element.included
+                    elif isinstance(element, RedundancyChoice):
+                        json_choice['type'] = 'RedundancyChoice'
+                        json_choice['n']    = int(element.n)
+                    else:
+                        raise ValueError('Unknown choice %s' % element)
+                    json_choices[self.graph.nodes.get(id=choice.key).client_id] = json_choice
+                current_config['choices'] = json_choices
+                json_config.append(current_config)
+
+            json_result['configurations'] = json_config
+            return_data                   = json.dumps(json_result)
+            logger.debug('Returning result JSON to frontend:\n' + return_data)
+
+            return return_data
+
+        except Exception as e:
+            mail_managers('Error on analysis result XML->JSON conversion', '%s\n\n%s' % (str(result_data), str(e),))
+            raise HttpResponseServerErrorAnswer()
 
 @receiver(post_save, sender=Job)
 def job_post_save(sender, instance, created, **kwargs):
@@ -113,9 +151,9 @@ def job_post_save(sender, instance, created, **kwargs):
         # by the test suite run accordingly
 
         #TODO: job_files_url = reverse('job_files', kwargs={'job_secret': instance.secret})
-        job_files_url = settings.SERVER + '/api/jobs/'+instance.secret+'/'
+        job_files_url = '%s/api/jobs/%s/' % (settings.SERVER, instance.secret,)
+
         cursor = connection.cursor()
-        cursor.execute("NOTIFY %s, '%s';"%(instance.kind, job_files_url))
+        cursor.execute("NOTIFY %s, '%s';" % (instance.kind, job_files_url,))
         #print "Notification to: "+str(cursor.db.connection.dsn)
         transaction.commit_unless_managed()
-
