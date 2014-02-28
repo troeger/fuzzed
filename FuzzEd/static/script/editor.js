8e798174e1c87909e6d2525496c118e829cf4030
cwerling <crwerling@gmail.com>
2014-02-28 10:59:18 +0100

    Platform specific shortcut hints in Edit menu (e.g. "Ctrl+X"/"⌘X")

--- FuzzEd/static/script/editor.js
+++ FuzzEd/static/script/editor.js
@@ -222,6 +222,14 @@ function(Class, Menus, Canvas, Backend, Alerts, Progress) {
                 this._selectAll(event);
             }.bind(this));
 
+            // set the shortcut hints from "Ctrl+" to "⌘" when on Mac
+            if (navigator.platform == "MacIntel" || navigator.platform == "MacPPC") {
+                jQuery("#"+this.config.IDs.ACTION_CUT+" span").text("⌘X");
+                jQuery("#"+this.config.IDs.ACTION_COPY+" span").text("⌘C");
+                jQuery("#"+this.config.IDs.ACTION_PASTE+" span").text("⌘P");
+                jQuery("#"+this.config.IDs.ACTION_SELECTALL+" span").text("⌘A");
+            }
+
             return this;
         },
 
