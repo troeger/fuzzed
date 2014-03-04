0ac166d4b73b1c3e2024cb0451476c352da40948
troeger <peter@troeger.eu>
2014-03-04 22:14:30 +0100

    Fixing broken code in default temporarily

--- FuzzEd/api.py
+++ FuzzEd/api.py
@@ -22,7 +22,7 @@ from FuzzEd.decorators import require_ajax
 from FuzzEd.middleware import HttpResponse, HttpResponseNoResponse, HttpResponseBadRequestAnswer, \
                               HttpResponseForbiddenAnswer, HttpResponseCreated, HttpResponseNotFoundAnswer, \
                               HttpResponseServerErrorAnswer
-from FuzzEd.models import Graph, Node, notations, commands, Job, Notification, Result
+from FuzzEd.models import Graph, Node, notations, commands, Job, Notification
 
 import logging, json
 logger = logging.getLogger('FuzzEd')
@@ -560,9 +560,6 @@ def job_files(request, job_secret):
             # Retrieve binary file and store it
             assert(len(request.FILES.values())==1)
             job.result = request.FILES.values()[0].read()
-            
-            result = Result.Result.createFromXML(request.FILES.values()[0].read())
-            
             job.exit_code = 0       # This saves as a roundtrip. Having files means everything is ok.
             job.save()
             if not job.requires_download():
