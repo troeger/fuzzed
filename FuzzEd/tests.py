1300a1cadf1b267d16fd12cec64fd141ecb51a77
troeger <peter@troeger.eu>
2014-04-10 07:20:49 -0400

    More analysis testing, job debugging in admin

--- FuzzEd/tests.py
+++ FuzzEd/tests.py
@@ -320,13 +320,22 @@ class AnalysisInputFilesTestCase(FuzzEdTestCase):
         They only test if the analysis engine crashes on them.
         The may later be translated to real tests with some expected output.
     '''
-    def testFiles(self):
+    def testFileAnalysis(self):
         for root, dirs, files in os.walk('FuzzEd/fixtures/analysis'):
             for f in files:
                 fname = root+os.sep+f
                 print "Testing "+fname
                 retcode = subprocess.call('backends/lib/ftanalysis_exe %s /tmp/output.xml /tmp'%(fname), shell=True)
-                self.assertEqual(retcode, 0)
+                self.assertEqual(retcode, 0, fname+ " failed")
+                dom = parse('/tmp/output.xml')
+
+    def testFileSimulation(self):
+        for root, dirs, files in os.walk('FuzzEd/fixtures/analysis'):
+            for f in files:
+                fname = root+os.sep+f
+                print "Testing "+fname
+                retcode = subprocess.call('backends/lib/ftsimulation %s /tmp/output.xml /tmp'%(fname), shell=True)
+                self.assertEqual(retcode, 0, fname+ " failed")
                 dom = parse('/tmp/output.xml')
 
 class AnalysisFixtureTestCase(FuzzEdTestCase):
