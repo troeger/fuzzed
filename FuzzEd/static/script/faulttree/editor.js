57094bdcdcb682974886c8058ea64c8799b8d958
cwerling <crwerling@gmail.com>
2014-05-14 15:05:43 +0200

    notations modifications; setup of group and ungroup menu actions until we have halos

--- FuzzEd/static/script/faulttree/editor.js
+++ FuzzEd/static/script/faulttree/editor.js
@@ -1062,6 +1062,68 @@ function(Editor, Canvas, FaulttreeGraph, Menus, FaulttreeConfig, Alerts) {
             //TODO: maybe we can use more sophisticated methods here to get the file to download directly instead
             //      of opening in the same window
             window.location = url;
+        },
+
+
+        /**
+         *      !! Temporarily copied code from dfd/editor.js !!
+         *      Will be used until halos are implemented and take care of FaultTree NodeGroups.
+         */
+
+        _setupMenuActions: function() {
+            this._super();
+
+            jQuery('#' + this.config.IDs.ACTION_GROUP).click(function() {
+                this._groupSelection();
+            }.bind(this));
+
+            jQuery('#' + this.config.IDs.ACTION_UNGROUP).click(function() {
+                this._ungroupSelection();
+            }.bind(this));
+
+            // set the shortcut hints from 'Ctrl+' to '⌘' when on Mac
+            if (navigator.platform == 'MacIntel' || navigator.platform == 'MacPPC') {
+                jQuery('#' + this.config.IDs.ACTION_GROUP + ' span').text('⌘G');
+                jQuery('#' + this.config.IDs.ACTION_UNGROUP + ' span').text('⌘U');
+            }
+
+
+            return this;
+        },
+
+        _setupKeyBindings: function(readOnly) {
+            this._super(readOnly)
+            if (readOnly) return this;
+
+            jQuery(document).keydown(function(event) {
+                if (event.which === 'G'.charCodeAt() && (event.metaKey || event.ctrlKey)) {
+                    this._groupPressed(event);
+                } else if (event.which === 'U'.charCodeAt() && (event.metaKey || event.ctrlKey)) {
+                    this._ungroupPressed(event);
+                }
+            }.bind(this));
+
+            return this;
+        },
+
+        _groupPressed: function(event) {
+            // prevent that node is being deleted when we edit an input field
+            if (jQuery(event.target).is('input, textarea')) return this;
+            event.preventDefault();
+
+            this._groupSelection();
+
+            return this;
+        },
+
+        _ungroupPressed: function(event) {
+            // prevent that node is being deleted when we edit an input field
+            if (jQuery(event.target).is('input, textarea')) return this;
+            event.preventDefault();
+
+            this._ungroupSelection();
+
+            return this;
         }
     });
 });
