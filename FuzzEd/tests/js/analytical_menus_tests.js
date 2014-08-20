9ec82be64d1627b05cfddd38293682b5a18db7f2
felixKubicek <felix.kubicek@gmail.com>
2014-08-20 11:23:20 +0200

    added polyfill for missing bind function in phantoms

--- FuzzEd/tests/js/analytical_menus_tests.js
+++ FuzzEd/tests/js/analytical_menus_tests.js
@@ -1,5 +1,5 @@
 define(['faulttree/analytical_menus'],
-function(AnalyticalMenus) {
+function(AnalyticalMenus, jquery) {
     
     describe('Analytical Menus',function(){    
         describe('Analytical Probability Menu', function(){
@@ -7,9 +7,15 @@ function(AnalyticalMenus) {
             var editor = undefined
             var apMenu = new AnalyticalMenus.AnalyticalProbabilityMenu(editor);
             
+            
+            beforeEach(function(){
+              console.log(jQuery.type(apMenu._chartContainer))
+            })
+            
+            
             describe('init()', function(){
                 it('should have set up all required containers after initialization', function(){
-                   assert(apMenu._chartContainer.is('div'), 'missing chart container') 
+                   assert(apMenu._chartContainer.is('div'), "missing chart container" ) 
                 });
             });
         });
