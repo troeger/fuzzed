725cde2eb3d626a2b9cbe25f6d655b7998d0d469
troeger <peter@troeger.eu>
2014-09-16 14:30:30 +0200

    Moving last Fabric parts to ansible

--- fabfile/common.py
+++
@@ -1,6 +0,0 @@
-import ConfigParser
-
-conf = ConfigParser.ConfigParser()
-conf.optionxform = str   # preserve case in keys    
-conf.read('./settings.ini')
-version=dict(conf.items('all'))['VERSION']
