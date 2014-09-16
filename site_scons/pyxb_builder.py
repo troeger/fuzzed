1b5040696cf0c1c2a8c57a9a7fda9c25daffce8a
troeger <peter@troeger.eu>
2014-09-16 08:41:16 +0200

    Trying to fix PyXB builder

--- site_scons/pyxb_builder.py
+++ site_scons/pyxb_builder.py
@@ -8,23 +8,17 @@ def build_xmlschema_wrappers(target, source, env):
         PyXBGen, however, wants no .py suffix, otherwise modules are generated.
         Therefore, we strip the suffix before calling PyXBGen.
     '''
-
-    IS_WINDOWS     = os.name == 'nt'
-    FILE_EXTENSION = '.py' if IS_WINDOWS else ''
-
     sources = [str(src) for src in source]
     targets = [str(trg).rsplit('.')[0] for trg in target]
     modulespec = ['-u %s -m %s'%(src, trg) for src, trg in zip(sources,targets)]
 
     # generate new bindings
-    cmdline = 'pyxbgen%(extension)s %(modulespec)s' % {
-        'extension': FILE_EXTENSION,
-        'modulespec'  : ' '.join(modulespec)
-    }
+    cmdline = 'pyxbgen '+' '.join(modulespec)
     print cmdline
     if os.system(cmdline) != 0:
         raise Exception('Execution of pyxbgen failed.')
 
 # Define Builder for Django config file from settings INI file
 pyxbbuilder = Builder(action = build_xmlschema_wrappers)
+
 
