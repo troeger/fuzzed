56cda31594bf4ab107360b9e6be371add5c425c9
Peter Tröger <peter@troeger.eu>
2015-04-21 08:18:17 +0200

    Preparing pre-release

--- FuzzEd/__init__.py
+++ FuzzEd/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-VERSION=0.67
+VERSION="0.68beta1"
 
 if "DJANGO_CONFIGURATION" not in os.environ:
     print "Please set the DJANGO_CONFIGURATION environment variable to either 'Dev', 'Vagrant' or 'Production'."
