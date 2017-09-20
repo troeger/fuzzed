ec08611c6a6b4f630e8e8e843c2f945c729fb36f
Peter Troeger <peter@troeger.eu>
2017-09-20 16:10:54 +0000

    Env vars on dev machines are annoying

--- FuzzEd/__init__.py
+++ FuzzEd/__init__.py
@@ -2,6 +2,3 @@ import os
 
 VERSION="0.7.5"
 
-if "DJANGO_CONFIGURATION" not in os.environ:
-    print "Please set the DJANGO_CONFIGURATION environment variable to either 'Dev', 'Vagrant' or 'Production'."
-    exit(-1)
