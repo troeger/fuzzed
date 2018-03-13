034c1649fc356e66d7e05bd86caa82443c2c2d34
Peter Tröger <peter@troeger.eu>
2018-03-13 14:52:32 +0100

    Update for Django 1.11

--- front/FuzzEd/urls.py
+++ front/FuzzEd/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.http import HttpResponse
 from django.contrib import admin
 from tastypie.api import Api
 
 from FuzzEd.api import external, frontend, backend
+from FuzzEd import views
 
 
 v1_api = Api(api_name='v1')
@@ -26,76 +26,27 @@ admin.autodiscover()
 
 re_uuid = "[0-F]{8}-[0-F]{4}-[0-F]{4}-[0-F]{4}-[0-F]{12}"
 
-urlpatterns = patterns('',
-                       # admin
-                       url(r'^admin/doc/',
-                           include('django.contrib.admindocs.urls')),
-                       url(r'^admin/', include(admin.site.urls)),
+urlpatterns = [
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^settings/$', views.settings, name='settings'),
+    url(r'^editor/(?P<graph_id>\d+)$', views.editor, name='editor'),
+    url(r'^snapshot/(?P<graph_id>\d+)$', views.snapshot, name='snapshot'),
+    url(r'^projects/$', views.projects, name='projects'),
+    url(r'^projects/new/$', views.project_new, name='project_new'),
+    url(r'^projects/(?P<project_id>\d+)/$', views.project_edit, name='project_edit'),
+    url(r'^projects/(?P<project_id>\d+)/dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^projects/(?P<project_id>\d+)/dashboard/new/(?P<kind>\w{1,50})$', views.dashboard_new, name='dashboard_new'),
+    url(r'^projects/(?P<project_id>\d+)/dashboard/import/', views.dashboard_import, name='dashboard_import'),
+    url(r'^projects/(?P<project_id>\d+)/dashboard/edit/', views.dashboard_edit, name='dashboard_edit'),
+    url(r'^projects/shared_graphs_dashboard', views.shared_graphs_dashboard, name='shared_graphs_dashboard'),
+    url(r'^graphs/(?P<graph_id>\d+)/', views.graph_settings, name='graph_settings'),
+    url(r'^robots\.txt', include('robots.urls')),
+    url(r'^api/', include(v1_api.urls + front_api.urls + back_api.urls)),
+    url(r'', include('social_django.urls', namespace='social'))
+]
 
-                       # Django Frontend Views
-                       url(r'^$', 'FuzzEd.views.index', name='index'),
-                       url(r'^login/$', 'FuzzEd.views.login', name='login'),
-                       url(r'^about/$', 'FuzzEd.views.about', name='about'),
-                       url(r'^settings/$',
-                           'FuzzEd.views.settings',
-                           name='settings'),
-
-                       url(r'^editor/(?P<graph_id>\d+)$',
-                           'FuzzEd.views.editor',
-                           name='editor'),
-                       url(r'^snapshot/(?P<graph_id>\d+)$',
-                           'FuzzEd.views.snapshot',
-                           name='snapshot'),
-
-                       url(r'^projects/$',
-                           'FuzzEd.views.projects',
-                           name='projects'),
-                       url(r'^projects/new/$',
-                           'FuzzEd.views.project_new',
-                           name='project_new'),
-                       url(r'^projects/(?P<project_id>\d+)/$',
-                           'FuzzEd.views.project_edit',
-                           name='project_edit'),
-                       url(r'^projects/(?P<project_id>\d+)/dashboard/$',
-                           'FuzzEd.views.dashboard',
-                           name='dashboard'),
-                       url(
-                           r'^projects/(?P<project_id>\d+)/dashboard/new/(?P<kind>\w{1,50})$',
-                           'FuzzEd.views.dashboard_new',
-                           name='dashboard_new'),
-                       url(r'^projects/(?P<project_id>\d+)/dashboard/import/',
-                           'FuzzEd.views.dashboard_import',
-                           name='dashboard_import'),
-                       url(r'^projects/(?P<project_id>\d+)/dashboard/edit/',
-                           'FuzzEd.views.dashboard_edit',
-                           name='dashboard_edit'),
-                       url(r'^projects/shared_graphs_dashboard',
-                           'FuzzEd.views.shared_graphs_dashboard',
-                           name='shared_graphs_dashboard'),
-                       url(r'^graphs/(?P<graph_id>\d+)/',
-                           'FuzzEd.views.graph_settings',
-                           name='graph_settings'),
-
-                      # robots.txt via django-robots
-                      url(r'^robots\.txt$', include('robots.urls')),
-
-                       # API
-                       url(r'^api/',
-                           include(v1_api.urls + front_api.urls + back_api.urls)),
-
-                      # Django Social Auth
-                       url(r'', include('social.apps.django_app.urls', namespace='social'))
-
-                       )
 urlpatterns += staticfiles_urlpatterns()
-
-# Some debugging code that shows the final complete list of all configured
-# URL's
-
-
-def show_urls(urllist, depth=0):
-    for entry in urllist:
-        print(("  " * depth, entry.regex.pattern))
-        if hasattr(entry, 'url_patterns'):
-            show_urls(entry.url_patterns, depth + 1)
-#show_urls(urlpatterns)
