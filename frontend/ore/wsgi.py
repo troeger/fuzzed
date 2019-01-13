b384878784412a391009f5d378c37633f845027c
Peter Tröger <peter@troeger.eu>
2019-01-13 15:09:53 +0100

    Fix WSGI loading

--- frontend/ore/wsgi.py
+++ frontend/ore/wsgi.py
@@ -1,5 +1,6 @@
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ore.settings')
+
 from configurations.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ore.settings')
 application = get_wsgi_application()
